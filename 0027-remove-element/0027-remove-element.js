a482bccc3e5646defd42ceec1b97e7af603c7a30
Victor Zarour <victor.zarour@gmail.com>
2022-10-11 15:25:42 -0400

    Time: 81 ms (73.81%), Space: 42.2 MB (32.78%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -5,23 +5,30 @@
  */
 var removeElement = function(nums, val) {
     
-    let low = 0
-    let high = nums.length - 1
+//     let low = 0
+//     let high = nums.length - 1
     
-    // if (nums.length === 1 && nums[0] !== val) return nums
-    // if (nums.length === 1 && nums[0] == val) return []
+//     while (low <= high){
+//         if (nums[low] == val && nums[high] !== val){
+//             [nums[low], nums[high]] = [nums[high], nums[low]] 
+//             low++
+//             high--
+//         } 
+//         if (nums[low] !== val && nums[high] !== val) low++
+//         if (nums[low] == val && nums[high] == val) high--
+//         if (nums[low] !== val && nums[high] == val) high--
+//     }
+//     if (low === high) return nums
+//     else return low
     
-    while (low <= high){
-        if (nums[low] == val && nums[high] !== val){
-            [nums[low], nums[high]] = [nums[high], nums[low]] 
-            low++
-            high--
+    let i = 0
+    
+    for (let j = 0; j < nums.length; j++){
+        if (nums[j] !== val){
+            nums[i] = nums[j]
+            i++
         } 
-        if (nums[low] !== val && nums[high] !== val) low++
-        if (nums[low] == val && nums[high] == val) high--
-        if (nums[low] !== val && nums[high] == val) high--
     }
-    if (low === high) return nums
-    else return low
     
+    return i 
 };
