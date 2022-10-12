908c937a2fb50cb6942352c29b79f5b1f27a7ff6
Victor Zarour <victor.zarour@gmail.com>
2022-10-12 13:24:19 -0400

    Time: 97 ms (46.08%), Space: 42.3 MB (26.77%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    
+    for (var i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++
+            break;
+        } else if (i === 0) {
+            digits[0] = 0
+            digits = [1].concat(digits)
+            break;
+        } else {
+            digits[i] = 0
+        }
+    }
+    return digits
+    
+};
