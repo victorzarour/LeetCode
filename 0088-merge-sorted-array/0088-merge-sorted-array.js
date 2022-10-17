93c983b5a81207fb397401584c095f5ac9d5f230
Victor Zarour <victor.zarour@gmail.com>
2022-10-17 16:44:07 -0400

    Time: 98 ms (48.31%), Space: 41.7 MB (94.44%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -15,11 +15,11 @@ var merge = function(nums1, m, nums2, n) {
     let p1 = m - 1
     let p2 = n - 1
     
-    for (let i = m + n - 1; i >= 0; i--){
-        if (p1 >= 0 && p2 >= 0) nums1[i] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--]
+    for (let i = m + n - 1; i >= 0; i --){
+        if (p1 >= 0 && p2 >=0) nums1[i] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--]
         else if (p1 >= 0) nums1[i] = nums1[p1--]
         else nums1[i] = nums2[p2--]
-    }    
-          
+    }
+
     return;
 };
