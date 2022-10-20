abcdeaab2f9ea500fc7c921a3ba218518a971ce8
Victor Zarour <victor.zarour@gmail.com>
2022-10-20 15:09:26 -0400

    Time: 125 ms (80.33%), Space: 44.3 MB (98.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -5,20 +5,21 @@
 
 const lengthOfLongestSubstring = (s) => {
 
-    let tempMax = 0 
-    let maxLen = 0;
-    const map = new Map();   
-  
-    for(let i = 0; i < s.length; i++) { 
-        const char = s[i]; 
-        if (map.get(char) >= tempMax) { 
-            tempMax = map.get(char) + 1; 
+    let maxLength = 0
+    let start = 0
+    const map = new Map()
+    
+    for (let i = 0; i < s.length; i++){
+        const char = s[i]
+        
+        if (map.get(char) >= start){
+            start = map.get(char) + 1
         }
-
-        map.set(char, i); 
-      
-        maxLen = Math.max(maxLen, i - tempMax + 1) 
+                 
+        map.set(char, i)
+        
+        maxLength = Math.max(maxLength, i - start + 1)
     }
-
-    return maxLen
+    
+    return maxLength
 }
