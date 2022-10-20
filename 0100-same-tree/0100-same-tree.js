c1c370de40ce9670b32021d1d01fc1b8b50cb818
Victor Zarour <victor.zarour@gmail.com>
2022-10-20 13:50:40 -0400

    Time: 91 ms (59.34%), Space: 42.6 MB (24.10%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,11 +12,12 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-
-    if (p == null && q == null) return true;
-    if (p == null || q == null) return false;
     
-    if (p.val != q.val) return false;
+    if (p == null && q == null) return true
+    if (p == null || q == null) return false
+    
+    if (p.val !== q.val) return false
     
     return isSameTree(p.right, q.right) && isSameTree(p.left, q.left)
+
 };
