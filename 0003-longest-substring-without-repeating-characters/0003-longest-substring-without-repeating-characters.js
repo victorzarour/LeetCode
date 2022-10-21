f6d326f36bdf2983765420c79def6f9f78e449d3
Victor Zarour <victor.zarour@gmail.com>
2022-10-21 06:08:42 -0400

    Time: 71 ms (99.73%), Space: 44.3 MB (98.73%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -4,24 +4,23 @@
  */
 
 const lengthOfLongestSubstring = (s) => {
-
+    
     let max = 0
     let start = 0
-    
     const map = new Map()
     
-    for (let i = 0; i < s.length; i++){
-        const char = s[i]
+    for (let end = 0; end < s.length; end ++){
+        const char = s[end]
         
         if (map.get(char) >= start){
-            start = map.get(char) + 1
-        }
-        
-        map.set(char, i)
-        
-        max = Math.max(max, i + 1 - start)       
-        
+        start = map.get(char) + 1
     }
+         
+        map.set(char, end)
+         
+        max = Math.max(max, end + 1 - start)
+    }
+
     
     
     return max
