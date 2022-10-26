60e8981d092482a0346eac4b2675d564f9a6b34d
Victor Zarour <victor.zarour@gmail.com>
2022-10-25 20:49:41 -0400

    Time: 276 ms (81.70%), Space: 99.6 MB (19.73%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(r) {
+        if(!r)
+        return 0;
+        if((!r.left&&r.right)||(r.left&&!r.right))
+        return Math.max(1+minDepth(r.left),1+minDepth(r.right))
+        return Math.min(1+minDepth(r.left),1+minDepth(r.right))
+};
