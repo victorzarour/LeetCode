be3ce760c3c9d6b9492ec68aaf5da301ca3464b4
Victor Zarour <victor.zarour@gmail.com>
2022-11-03 16:43:47 -0400

    Time: 90 ms (70.61%), Space: 43.3 MB (26.41%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -3,25 +3,44 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
+   
+    let object = {}
     
-    let map = new Map()
-    
-    for (num of arr){
-        if (!map.get(num)) map.set(num, 1)
-        else map.set(num, map.get(num) + 1)
-    }
-
-    let array = []
-    
-    for (num of map.values()){
-        array.push(num)
+    for (let num of arr){
+        object[num] = object[num] ? ++object[num] : 1
     }
     
-    let sorted = array.sort()
+    let sorted = Object.values(object).sort()
     
     for (let i = 0; i < sorted.length; i++){
         if (sorted[i] === sorted[i + 1]) return false
     }
     
     return true
+    
+    
+    
+    
+    
+    
+//     let map = new Map()
+    
+//     for (num of arr){
+//         if (!map.get(num)) map.set(num, 1)
+//         else map.set(num, map.get(num) + 1)
+//     }
+
+//     let array = []
+    
+//     for (num of map.values()){
+//         array.push(num)
+//     }
+    
+//     let sorted = array.sort()
+    
+//     for (let i = 0; i < sorted.length; i++){
+//         if (sorted[i] === sorted[i + 1]) return false
+//     }
+    
+//     return true
 };
