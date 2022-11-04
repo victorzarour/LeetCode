0cf1a09139f585b2745abbba69204ab97e1956c5
Victor Zarour <victor.zarour@gmail.com>
2022-11-04 10:05:17 -0400

    Time: 365 ms (18.17%), Space: 50.9 MB (54.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,7 +3,16 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let set = new Set(nums)
+//     let set = new Set(nums)
     
-    return !(set.size === nums.length) 
+//     return !(set.size === nums.length) 
+    
+    let sorted = nums.sort()
+    
+    for (let i = 0; i < sorted.length; i++){
+        if (sorted[i] === sorted[i + 1]) return true
+    }
+    
+    return false
+    
 };
