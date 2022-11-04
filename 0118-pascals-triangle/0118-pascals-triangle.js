b9b2f45d02ee07e2f7dce7d6342f8a074538b40f
Victor Zarour <victor.zarour@gmail.com>
2022-11-04 11:12:56 -0400

    Time: 76 ms (78.41%), Space: 42.1 MB (46.99%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let array = []
+    
+    for (let i = 0; i < numRows; i++){
+        let row = new Array(i + 1)
+        row[0] = 1
+        row[row.length - 1] = 1
+        
+        for (let j = 1; j < row.length - 1; j++){
+            let rowAbove = array[i - 1]
+            row[j] = rowAbove[j] + rowAbove[j - 1]
+        }
+        array[i] = row
+    }
+    
+    return array
+};
