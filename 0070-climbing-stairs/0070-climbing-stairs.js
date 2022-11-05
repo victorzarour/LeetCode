1667da2d611ec0012abc91463efc4463202c4f66
Victor Zarour <victor.zarour@gmail.com>
2022-11-05 09:28:33 -0400

    Time: 97 ms (41.21%), Space: 42.4 MB (7.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -4,9 +4,9 @@
  */
 var climbStairs = function(n) {
 
-    const memo = [1, 1, 2];
+    const memo = [1, 1];
     
-    for (let i = 3; i <= n; i++){
+    for (let i = 2; i <= n; i++){
         memo[i] = memo[i - 1] + memo[i - 2];
     }
     
