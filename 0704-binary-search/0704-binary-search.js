415139aca9ff628491d8c4692a881d35847e9fd5
Victor Zarour <victor.zarour@gmail.com>
2022-11-05 08:37:27 -0400

    Time: 98 ms (67.76%), Space: 44.6 MB (91.09%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -5,9 +5,20 @@
  */
 var search = function(nums, target) {
     
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i] === target) return i        
-    }
+//     for (let i = 0; i < nums.length; i++){
+//         if (nums[i] === target) return i        
+//     }
     
-    return -1
+//     return -1
+    
+    let beginning = 0
+    let end = nums.length - 1
+    
+    while (beginning <= end){
+        let mid = Math.floor((end + beginning) / 2)
+        if (nums[mid] === target) return nums.indexOf(target)
+        if (target > nums[mid]) beginning = mid + 1
+        if (target < nums[mid]) end = mid - 1
+    }
+    return -1    
 };
