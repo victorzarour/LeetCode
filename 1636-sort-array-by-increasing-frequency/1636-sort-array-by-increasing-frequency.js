ffd173f77297bb0a92a6bd6a9c524b6a578ec4db
Victor Zarour <victor.zarour@gmail.com>
2022-11-07 11:27:41 -0500

    Time: 85 ms (91.57%), Space: 44.2 MB (88.76%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var frequencySort = function(nums) {
+        let map = new Map()
+    
+    for (let num of nums){
+        if (map.get(num)) map.set(num, map.get(num) + 1)
+        else map.set(num, 1)
+    }
+    
+    return nums.sort((a,b)=>{
+        if(map.get(a) === map.get(b)){
+          return b-a
+        }else{
+          return map.get(a) - map.get(b)
+        }
+      })
+    
+};
