7e3f3f0eec6a416dae32d3111de6bb1977e38f11
Victor Zarour <victor.zarour@gmail.com>
2022-11-08 17:25:17 -0500

    Time: 213 ms (32.16%), Space: 46.7 MB (66.08%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -3,6 +3,6 @@
  * @return {number}
  */
 var maximumProduct = function(nums) {
-    nums = nums.sort((a,b) => b-a)
-    return Math.max(nums[0] * nums[1] * nums[2], nums[nums.length-1] * nums[nums.length-2] * nums[0])
+    nums = nums.sort((a,b) => a-b)
+    return Math.max(nums[0] * nums[1] * nums[nums.length -1], nums[nums.length-1] * nums[nums.length-2] * nums[nums.length -3])
 };
