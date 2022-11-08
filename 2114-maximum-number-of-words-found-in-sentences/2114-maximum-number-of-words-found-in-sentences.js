32007a24a9d99c762a9fe3c2d0836dd64b392226
Victor Zarour <victor.zarour@gmail.com>
2022-11-08 17:55:36 -0500

    Time: 92 ms (73.57%), Space: 43.6 MB (88.62%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -4,17 +4,24 @@
  */
 var mostWordsFound = function(sentences) {
     
+//     let max = 0
+//     let count = 0
+    
+//     for (let sentence of sentences){
+//         count = 0
+//         for (let char of sentence){
+//             if (char === " ") count++
+//         }
+//         max = Math.max(max, count + 1)
+//     }
+    
+//     return max
+    
     let max = 0
-    let count = 0
     
     for (let sentence of sentences){
-        count = 0
-        for (let char of sentence){
-            if (char === " ") count++
-        }
-        max = Math.max(max, count + 1)
+        max = Math.max(max, sentence.split(" ").length)
     }
     
     return max
-    
 };
