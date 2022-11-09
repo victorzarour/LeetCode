4c15eedc313cd7807dcbcf8e581a2ce57e5f9358
Victor Zarour <victor.zarour@gmail.com>
2022-11-09 17:20:16 -0500

    Time: 239 ms (53.85%), Space: 55.2 MB (54.00%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function (fruits) {
+	let max = 0, l = 0, size = 0, map = {}
+	fruits.forEach((fruit, i) => {
+		map[fruit] ? map[fruit]++ : (map[fruit] = 1, size++)
+		while (size > 2) {
+			const tail = fruits[l]
+			if (--map[tail] === 0) delete map[tail], size--
+			l++
+		}
+		max = Math.max(max, i - l + 1)
+	})
+	return max
+}
