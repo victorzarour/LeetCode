1f67df65cbf582864e8b55689b24a9f6789ce9ca
Victor Zarour <victor.zarour@gmail.com>
2022-11-09 11:01:36 -0500

    Time: 63 ms (95.83%), Space: 42.5 MB (58.33%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -4,9 +4,23 @@
  * @return {boolean}
  */
 var search = function(nums, target) {
-        for (let num of nums){
-        if (num === target) return true
-    }
-    
-    return false
+    let start=0, end=nums.length-1; 
+    while (start <= end) {
+        let mid=Math.floor((start + end)/2)
+        if (nums[mid]===target) 
+            return true
+        else if (nums[mid] == nums[start] && nums[mid] == nums[end]) {
+            start++
+            end--
+        }
+        else if (nums[start] <= nums[mid])  {
+            if (nums[start] <= target && nums[mid] > target) end = mid - 1
+            else start = mid + 1
+        }
+        else {
+            if (nums[mid] < target && nums[end] >= target) start = mid + 1
+            else end = mid - 1
+        }
+    } 
+    return false; 
 };
