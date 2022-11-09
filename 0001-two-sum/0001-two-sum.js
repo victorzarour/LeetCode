6c2bc39000bd463213f43186abb94ed679948e5b
Victor Zarour <victor.zarour@gmail.com>
2022-11-09 16:02:58 -0500

    Time: 76 ms (93.50%), Space: 43.4 MB (20.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,9 +5,17 @@
  */
 var twoSum = function(nums, target) {
     
+    // for (let i = 0; i < nums.length; i++){
+    //     for (let j = 1; j < nums.length && i !== j; j++){
+    //         if (nums[i] + nums[j] === target) return [i, j]
+    //     }
+    // }
+    
+    let map = new Map()
+    
     for (let i = 0; i < nums.length; i++){
-        for (let j = 1; j < nums.length && i !== j; j++){
-            if (nums[i] + nums[j] === target) return [i, j]
-        }
+        let needed = target - nums[i]
+        if (map.has(needed)) return [map.get(needed), i]
+        else (map.set(nums[i], i))
     }
 };
