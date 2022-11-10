daadb34f0c47a28fb446133b1e40924c7b4886ad
Victor Zarour <victor.zarour@gmail.com>
2022-11-10 18:07:21 -0500

    Time: 147 ms (20.00%), Space: 46.5 MB (7.35%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,7 +3,19 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let ans = [...nums, ...nums]
+//     let ans = [...nums, ...nums]
+    
+//     return ans
+    
+    let ans = []
+    let count = 2
+    
+    while (count){
+        for (let num of nums){
+            ans.push(num)   
+        }
+        count--
+    }
     
     return ans
 };
