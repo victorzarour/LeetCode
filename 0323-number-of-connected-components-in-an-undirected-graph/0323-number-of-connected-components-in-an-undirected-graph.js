784d92796dd60757c436adebcde7ec7d4ed0a90e
Victor Zarour <victor.zarour@gmail.com>
2022-11-11 10:29:53 -0500

    Time: 89 ms (89.08%), Space: 48 MB (21.98%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.js
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.js
@@ -4,32 +4,49 @@
  * @return {number}
  */
 var countComponents = function(n, edges) {
-var count = 0;
-var isVisited = [];
-var graph = [];
-for(var j = 0; j < n; ++j) {
-    isVisited.push(false);
-    graph.push([]);
-}
-for(j = 0; j < edges.length; ++j) {
-    var one = edges[j][0];
-    var two = edges[j][1];
-    graph[one].push(two);
-    graph[two].push(one);
-}
-for(var i = 0; i < n; ++i) {
-    if(isVisited[i]) continue;
-    dfs(i, isVisited, graph);
-    count++;
-}
-return count;
+    
+    const graph = createGraph(edges)
+    const set = new Set()
+    let count = 0
+    let nodeCount = 0
+
+    for (let node in graph){
+    
+        nodeCount++
+        if (!set.has(String(node))) count++
+
+        let stack = [ node ]
+
+          while (stack.length){
+              
+            const current = stack.pop()
+            
+            set.add(String(current))
+
+              for (let neighbor of graph[current]){
+                if (!set.has(String(neighbor))) stack.push(neighbor)
+              }
+          }
+    }
+
+    return n === nodeCount ? count : count + (n - nodeCount)
+
+    
+    
+    
 };
 
-function dfs(node, isVisited, graph) {
-isVisited[node] = true;
-var nodes = graph[node];
-for(var i = 0; i < nodes.length; ++i) {
-    if(isVisited[nodes[i]]) continue;
-    dfs(nodes[i], isVisited, graph);
-}
+const createGraph = (edges) => {
+    let graph = {}
+    
+    for (let edge of edges){
+        const [ a, b ] = edge
+        
+        if (!graph[a]) graph[a] = []
+        if (!graph[b]) graph[b] = []
+        graph[a].push(b)
+        graph[b].push(a)        
+    }
+    
+    return graph
 }
