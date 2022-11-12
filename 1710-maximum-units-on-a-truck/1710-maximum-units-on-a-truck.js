348fd6f78ed8335ef2e8bc8a856a0a13e12d574a
Victor Zarour <victor.zarour@gmail.com>
2022-11-12 12:11:59 -0500

    Time: 173 ms (19.56%), Space: 48.9 MB (13.82%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} boxTypes
+ * @param {number} truckSize
+ * @return {number}
+ */
+var maximumUnits = function(boxTypes, truckSize) {
+    let unitCount = 0
+    let boxCount = truckSize    
+    
+   
+    const sortedMap = boxTypes.sort((a, b) => b[1] - a[1]);
+    
+    console.log(sortedMap)
+    
+    for (let box of sortedMap){
+        if (boxCount - box[0] >= 0){
+            unitCount += (box[0] * box[1])
+            boxCount -= box[0]
+        }
+        else {
+            while (boxCount > 0){
+            unitCount += box[1]
+            boxCount -= 1
+            }
+        }
+        
+    }
+    return unitCount
+};
