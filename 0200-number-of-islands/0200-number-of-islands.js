6e69a6c7020608db5c1a20ae886393bfd3e26499
Victor Zarour <victor.zarour@gmail.com>
2022-11-13 11:19:49 -0500

    Time: 132 ms (65.54%), Space: 45.3 MB (53.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,34 +3,29 @@
  * @return {number}
  */
 
-var numIslands = function(grid) {
-  let count = 0
-  const visited = new Set()
+function numIslands(grid) {
+  const H = grid.length;
+  const W = H && grid[0].length;
+  let count = 0;
   
-  for (let r = 0; r < grid.length; r++){
-    for (let c = 0; c < grid[r].length; c++){
-      if (explore(grid, r, c, visited)) count++
+  for (let r = 0; r < H; r++) {
+    for (let c = 0; c < W; c++) {
+      if (grid[r][c] === '0') continue;
+      
+      count++;
+      dfs(r, c);
     }
   }
-  return count
-};
-
-const explore = (grid, r, c, visited) => {
-  const rowLimit = r >= 0 && r < grid.length
-  const columnLimit = c >= 0 && c < grid[0].length
-   if (!rowLimit || !columnLimit) return false;
+  return count;
   
-  if (grid[r][c] === "0") return false
-  
-  const pos = `${r}, ${c}`
-  
-  if (visited.has(pos)) return false
-  visited.add(pos)
-  
-  explore(grid, r + 1, c, visited)
-  explore(grid, r - 1, c, visited)
-  explore(grid, r, c + 1, visited)
-  explore(grid, r, c - 1, visited)
-  
-  return true
+  function dfs(r, c) {
+    if (r < 0 || c < 0 || r === H || c === W) return;
+    if (grid[r][c] === '0') return;
+    
+    grid[r][c] = '0';
+    dfs(r-1, c);
+    dfs(r+1, c);
+    dfs(r, c-1);
+    dfs(r, c+1);
+  }
 }
