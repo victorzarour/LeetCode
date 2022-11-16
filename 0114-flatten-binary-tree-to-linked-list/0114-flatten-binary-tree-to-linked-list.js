ac1901c546b02ed4ecb3d9c9acd603cd446a1435
Victor Zarour <victor.zarour@gmail.com>
2022-11-16 07:10:16 -0500

    Time: 122 ms (41.74%), Space: 44.5 MB (64.35%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.js
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.js
@@ -11,7 +11,7 @@
  * @return {void} Do not return anything, modify root in-place instead.
  */
 var flatten = function(root) {
-    if (!root) return root
+    if (!root) return
         
     const stack = [ root ]
     let traversalNode = new TreeNode()
