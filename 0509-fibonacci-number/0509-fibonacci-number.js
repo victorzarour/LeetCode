3f6745df37a7e20b980d058c56473f0d90affe63
Victor Zarour <victor.zarour@gmail.com>
2022-11-17 18:48:18 -0500

    Time: 101 ms (51.96%), Space: 42.8 MB (5.65%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    const nums = [0, 1, 1]
+    if (n < nums.length) return nums[n]
+    
+    for (let i = 3; i <= n; i++){
+        nums[i] = nums[i - 1] + nums[i - 2]
+    }
+    console.log(nums)
+    return nums[nums.length - 1]
+};
