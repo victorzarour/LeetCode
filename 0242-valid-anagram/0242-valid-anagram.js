ade4e4af5f6a0749eed549c6ed5cc22fcd724097
Victor Zarour <victor.zarour@gmail.com>
2022-11-17 18:58:19 -0500

    Time: 126 ms (63.58%), Space: 45.2 MB (41.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    const map = new Map()
+    
+    for (let word of s){
+        map.set(word, map.get(word) + 1 || 1)
+    }
+    
+    for (let word of t){
+        if (map.get(word)) map.set(word, map.get(word) - 1)
+        else map.set(word, 1)
+    }
+    console.log(map)
+    
+    for (let count of map.values()){
+        if (count !== 0) return false
+    }
+    
+    return true
+    
+};
