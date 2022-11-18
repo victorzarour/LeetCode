cdbd5df0454a740308fee38cbfe56fbda967557e
Victor Zarour <victor.zarour@gmail.com>
2022-11-18 11:49:48 -0500

    Time: 233 ms (70.65%), Space: 50.1 MB (82.09%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.js
+++ 0934-shortest-bridge/0934-shortest-bridge.js
@@ -20,7 +20,7 @@ var shortestBridge = function (grid) {
 
 
 function dfs(grid, row, col, result) {
-    if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] != 1) return;
+    if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] === 0) return;
 
     grid[row][col] = 0;
     result.push([row, col])
