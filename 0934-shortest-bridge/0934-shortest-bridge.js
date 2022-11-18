33f84cce2c248509b705fc85f4cd190f79a21966
Victor Zarour <victor.zarour@gmail.com>
2022-11-18 12:58:30 -0500

    Time: 315 ms (51.24%), Space: 49.8 MB (84.58%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.js
+++ 0934-shortest-bridge/0934-shortest-bridge.js
@@ -3,33 +3,33 @@
  * @return {number}
  */
 var shortestBridge = function(grid) {
-    const firstIsland = []
-    const secondIsland = []
+    let firstIsland = []
+    let secondIsland = []
     
     for (let row = 0; row < grid.length; row++){
         for (let col = 0; col < grid[0].length; col++){
             if (grid[row][col] === 1){
                 if (!firstIsland.length) traverseGraph(grid, row, col, firstIsland)
-                else if (!secondIsland.length) traverseGraph(grid, row, col, secondIsland)     
+                else if (!secondIsland.length) traverseGraph(grid, row, col, secondIsland)
             }
         }
     }
-    return calculateDistance(firstIsland, secondIsland)
+    return calculateDifference(firstIsland, secondIsland)
 };
 
 const traverseGraph = (grid, row, col, island) => {
-    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === 0) return;
-   
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === 0) return
+    
     grid[row][col] = 0
     island.push([row, col])
     
-    traverseGraph(grid, row - 1, col, island)
     traverseGraph(grid, row + 1, col, island)
-    traverseGraph(grid, row, col - 1, island)
+    traverseGraph(grid, row - 1, col, island)
     traverseGraph(grid, row, col + 1, island)
+    traverseGraph(grid, row, col - 1, island)
 }
 
-const calculateDistance = (firstIsland, secondIsland) => {
+const calculateDifference = (firstIsland, secondIsland) => {
     let min = Infinity
     for (let i = 0; i < firstIsland.length; i++){
         for (let j = 0; j < secondIsland.length; j++){
