799b438356e5bffe424457e3d22465937b664f5c
Victor Zarour <victor.zarour@gmail.com>
2022-11-18 12:30:56 -0500

    Time: 307 ms (51.74%), Space: 49.5 MB (90.55%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.js
+++ 0934-shortest-bridge/0934-shortest-bridge.js
@@ -2,46 +2,41 @@
  * @param {number[][]} grid
  * @return {number}
  */
-
-var shortestBridge = function (grid) {
-    let aIsland = [];
-    let bIsland = [];
-
-    for (let row = 0; row < grid.length; row++) {
-        for (let col = 0; col < grid[0].length; col++) {
-            if (grid[row][col] === 1) {
-                if (!aIsland.length) dfs(grid, row, col, aIsland)
-                else if (!bIsland.length) dfs(grid, row, col, bIsland)
+var shortestBridge = function(grid) {
+    const firstIsland = []
+    const secondIsland = []
+    
+    for (let row = 0; row < grid.length; row++){
+        for (let col = 0; col < grid[0].length; col++){
+            if (grid[row][col] === 1){
+                if (!firstIsland.length) traverseGraph(grid, row, col, firstIsland)
+                else if (!secondIsland.length) traverseGraph(grid, row, col, secondIsland)     
             }
         }
     }
+    return calculateDistance(firstIsland, secondIsland)
+};
 
-    return calculateDistance(bIsland, aIsland)
+const traverseGraph = (A, i, j, result) => {
+        if (i < 0 || j < 0 || i >= A.length || j >= A.length || A[i][j] != 1) return;
+
+        //mark as visited
+        A[i][j] = 0;
+        result.push([i, j])
+
+        traverseGraph(A, i - 1, j, result);
+        traverseGraph(A, i + 1, j, result);
+        traverseGraph(A, i, j - 1, result);
+        traverseGraph(A, i, j + 1, result);
 }
 
-
-function dfs(grid, row, col, result) {
-    if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] === 0) return;
-
-    grid[row][col] = 0;
-    result.push([row, col])
-
-    dfs(grid, row - 1, col, result);
-    dfs(grid, row + 1, col, result);
-    dfs(grid, row, col - 1, result);
-    dfs(grid, row, col + 1, result);
-}
-
-function calculateDistance(aDistances, bDistance) {
-    let min = Infinity;
-
-    for (let i = 0; i < aDistances.length; i++) {
-        for (let j = 0; j < bDistance.length; j++) {
-            let calculateDiff = Math.abs(aDistances[i][0] - bDistance[j][0]) + Math.abs(aDistances[i][1] - bDistance[j][1]) - 1
-            min = Math.min(calculateDiff, min)
+const calculateDistance = (firstIsland, secondIsland) => {
+    let min = Infinity
+    for (let i = 0; i < firstIsland.length; i++){
+        for (let j = 0; j < secondIsland.length; j++){
+            let difference = Math.abs(firstIsland[i][0] - secondIsland[j][0]) + Math.abs(firstIsland[i][1] - secondIsland[j][1]) - 1
+            min = Math.min(min, difference)
         }
     }
     return min
 }
-
-    
