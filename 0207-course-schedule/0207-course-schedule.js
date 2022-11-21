59150b80c1c5459e6906b30891736be2435d7967
Victor Zarour <victor.zarour@gmail.com>
2022-11-21 11:07:04 -0500

    Time: 610 ms (5.04%), Space: 58.2 MB (5.54%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+    const graph = createGraph(prerequisites)
+    
+    for (let course in graph){
+        if (hasCycle(graph, new Set(), new Set(), course)) return false
+    }
+    
+    return true
+};
+
+const hasCycle = (graph, visited, visiting, course) => {
+    if (visited.has(course)) return false
+    if (visiting.has(course)) return true
+    visiting.add(course)
+    
+    for (let neighbor of graph[course]){
+        if (hasCycle(graph, visited, visiting, neighbor)) return true
+    }
+    
+    visiting.delete(course)
+    visited.add(course)
+}
+
+const createGraph = (prerequisites) => {
+    const graph = {}
+    
+    for (let course of prerequisites){
+        const [ a, b ] = course
+        if (!graph[a]) graph[a] = []
+        if (!graph[b]) graph[b] = []
+        graph[a].push(b)
+    }
+    
+    return graph
+}
