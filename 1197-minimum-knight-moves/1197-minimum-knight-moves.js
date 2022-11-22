9b0331b3014219fb0df80021ff112c547c8f9971
Victor Zarour <victor.zarour@gmail.com>
2022-11-22 15:30:02 -0500

    Time: 7687 ms (20.20%), Space: 140.4 MB (11.54%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.js
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.js
@@ -3,38 +3,38 @@
  * @param {number} y
  * @return {number}
  */
+
 var minKnightMoves = function(x, y) {
-  const visited = new Set()
-  visited.add(0, 0)
-  const queue = [ [0, 0, 0] ]
-  
-  while (queue.length){
-    const [ r, c, steps ] = queue.shift()
-    if (r === x && c === y) return steps
-    const neighbors = getNeighbors(r, c)
-    for (let neighbor of neighbors){
-      const [ row, col ] = neighbor
-      const position = `${row}, ${col}`
-      if (!visited.has(position)){
-        visited.add(position)
-        queue.push([row, col, steps + 1])
-      }
+    const visited = new Set()
+    visited.add(0, 0)
+    const queue = [ [0, 0, 0] ]
+    
+    while (queue.length){
+        const [ r, c, steps ] = queue.shift()
+        if (r === x && c === y) return steps
+        
+        const neighbors = getNeighbors(r, c) 
+        
+        for (let [ row, col ] of neighbors){
+            const position = `${row}, ${col}`
+            if (!visited.has(position)){
+                visited.add(position)
+                queue.push([row, col, steps + 1])
+            }
+        }
     }
-  }
-  return null
 };
 
 const getNeighbors = (r, c) => {
-  const positions = [
-    [r + 2, c + 1],
-    [r - 2, c + 1],
-    [r + 2, c - 1], 
-    [r - 2, c - 1],
-    [r + 1, c + 2],
-    [r - 1, c + 2],
-    [r + 1, c - 2], 
-    [r - 1, c - 2]
-  ]
-  
-  return positions
+    const positions = [
+        [r + 1, c + 2],
+        [r + 1, c - 2],
+        [r - 1, c + 2],
+        [r - 1, c - 2],
+        [r + 2, c + 1],
+        [r + 2, c - 1],
+        [r - 2, c + 1],
+        [r - 2, c - 1]
+    ]
+    return positions
 }
