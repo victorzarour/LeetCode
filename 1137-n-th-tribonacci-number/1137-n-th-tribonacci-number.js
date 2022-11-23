006b7c1a347ff667359b33e58c14cdf84db09202
Victor Zarour <victor.zarour@gmail.com>
2022-11-23 07:08:24 -0500

    Time: 91 ms (53.33%), Space: 42 MB (27.50%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -2,11 +2,21 @@
  * @param {number} n
  * @return {number}
  */
-var tribonacci = function(n, memo = {}) {
-    if (n === 0) return 0
-    if (n === 1 || n === 2) return 1
-    if (n in memo) return memo[n]
+// var tribonacci = function(n, memo = {}) {
+//     if (n === 0) return 0
+//     if (n === 1 || n === 2) return 1
+//     if (n in memo) return memo[n]
     
-    memo[n] = tribonacci(n - 3, memo) + tribonacci(n - 2, memo) + tribonacci(n - 1, memo)
-    return memo[n]
+//     memo[n] = tribonacci(n - 3, memo) + tribonacci(n - 2, memo) + tribonacci(n - 1, memo)
+//     return memo[n]
+// };
+
+var tribonacci = function(n) {
+    let arr = [0, 1, 1]
+    
+    for (let i = 3; i <= n; i++){
+        arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1]
+    }
+    
+    return arr[n]
 };
