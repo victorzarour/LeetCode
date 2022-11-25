226830b7a30eebf607338e47eb4c10216e6d251d
Victor Zarour <victor.zarour@gmail.com>
2022-11-25 07:55:33 -0500

    Time: 91 ms (56.92%), Space: 42.3 MB (13.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -2,16 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-    if (!nums.length) return 0
-    let prev1 = 0
-    let prev2 = 0
+// var rob = function(nums) {
+//     if (!nums.length) return 0
+//     let prev1 = 0
+//     let prev2 = 0
     
-    for (let num of nums){
-        const temp = prev1
-        prev1 = Math.max(prev2 + num, prev1)
-        prev2 = temp
-    }
-    return prev1
-};
+//     for (let num of nums){
+//         const temp = prev1
+//         prev1 = Math.max(prev2 + num, prev1)
+//         prev2 = temp
+//     }
+//     return prev1
+// };
+
+var rob = function(nums, i = 0, memo = {}) {
+    if (i >= nums.length) return 0
+    if (i in memo) return memo[i]
+    
+    const include = nums[i] + rob(nums, i + 2, memo)
+    const exclude = rob(nums, i + 1, memo)
+    
+    return memo[i] = Math.max(include, exclude)   
+}
 
