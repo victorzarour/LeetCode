6c1802fec84684da30cb8ea6079637206035b29e
Victor Zarour <victor.zarour@gmail.com>
2022-11-27 14:35:13 -0500

    Time: 4757 ms (5.00%), Space: 173.9 MB (5.01%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins, i = 0, memo = {}) {
+  const key = amount + ',' + i
+  if (key in memo) return memo[key]
+  if (amount === 0) return 1
+
+  const coin = coins[i]
+  
+  let total = 0
+  for (let qty = 0; qty * coin <= amount; qty++){
+    const remainder = amount - (qty * coin)
+    total += change(remainder, coins, i + 1, memo)
+  }
+  return memo[key] = total
+};
