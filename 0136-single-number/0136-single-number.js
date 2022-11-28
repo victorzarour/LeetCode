d8887df5e605ea9ec1e5facc48b8460668698b51
Victor Zarour <victor.zarour@gmail.com>
2022-11-28 11:08:50 -0500

    Time: 112 ms (60.24%), Space: 49.5 MB (8.31%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,10 +3,20 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    const sorted = nums.sort((a, b) => a-b)
+//     const sorted = nums.sort((a, b) => a-b)
     
-    for (let i = 0; i < sorted.length; i+= 2){
-        if (sorted[i] !== sorted[i+1]) return sorted[i]
+//     for (let i = 0; i < sorted.length; i+= 2){
+//         if (sorted[i] !== sorted[i+1]) return sorted[i]
+//     }
+    
+    const map = new Map()
+    
+    for (let num of nums){
+        map.set(num, map.get(num) + 1 || 1)
+    }
+    
+    for (let [key, val] of map){
+        if (val === 1) return key
     }
     
     
