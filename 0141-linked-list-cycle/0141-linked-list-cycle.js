ba54ed38e6f7956729c0163b806b2b91bc126570
Victor Zarour <victor.zarour@gmail.com>
2022-11-28 12:01:53 -0500

    Time: 143 ms (10.31%), Space: 46 MB (8.00%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let current = head
+    const set = new Set()
+    
+    while (current){
+        if (set.has(current)) return true
+        set.add(current)
+        current = current.next
+    }
+    
+    return false
+};
