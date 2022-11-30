37b47300fd7c58ac1d867b0de567db5317a68a9d
Victor Zarour <victor.zarour@gmail.com>
2022-11-30 11:51:26 -0500

    Time: 369 ms (47.72%), Space: 88.6 MB (58.51%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
@@ -6,14 +6,14 @@ var longestPalindromeSubseq = function(s) {
   
     const memo = [...Array(s.length)].map(() => Array(s.length).fill(0));
     
-    function run(i, j) {
-        if(i === j) return 1;
-        if(i > j) return 0;
-        if(memo[i][j]) return memo[i][j]
-        
-        if(s[i] === s[j]) return memo[i][j] = run(i+1, j-1) + 2;
-        else return memo[i][j] = Math.max(run(i+1, j), run(i, j-1));
+    function run(start, end) {
+      if (memo[start][end]) return memo[start][end]
+      if (start === end) return 1;
+      if (start > end) return 0;
 
+      if (s[start] === s[end]) return memo[start][end] = 2 + run(start + 1, end - 1)
+      else return memo[start][end] = Math.max(run(start + 1, end), run(start, end -1))
     }
+    
     return run(0, s.length-1);
 };
