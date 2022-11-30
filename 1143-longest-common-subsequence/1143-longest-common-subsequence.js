628640de9255857a0afe95c6ebb5173eb207cc20
Victor Zarour <victor.zarour@gmail.com>
2022-11-30 12:34:11 -0500

    Time: 419 ms (17.51%), Space: 43.7 MB (97.56%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -3,27 +3,27 @@
  * @param {string} text2
  * @return {number}
  */
-// var longestCommonSubsequence = function(text1, text2) {
-//     const m = text1.length;
-//     const n = text2.length;
-//     const memo = Array.from({ length: m }, () => new Uint16Array(n));
-    
-//     function find(i, j){
-//         if (memo[i][j]) return memo[i][j]
-//         if (i >= text1.length || j >= text2.length) return 0
-        
-//         if (text1[i] === text2[j]) return memo[i][j] = 1 + find(i + 1, j + 1)
-//         else return memo[i][j] = Math.max(find(i + 1, j), find(i, j + 1))
-    
-//     }
-//     return find(0, 0)
-// }
+var longestCommonSubsequence = function(text1, text2) {
+    const m = text1.length;
+    const n = text2.length;
+    const memo = Array.from({ length: m }, () => new Uint16Array(n));
 
-const longestCommonSubsequence = (str1, str2, i = 0, j = 0, memo = {}) => {
-  const key = i + ',' + j
-  if (key in memo) return memo[key]
-  if (i >= str1.length || j >= str2.length) return 0
-      
-  if (str1[i] === str2[j]) return memo[key] = 1 + longestCommonSubsequence(str1, str2, i + 1, j + 1, memo)
-  else return memo[key] = Math.max(longestCommonSubsequence(str1, str2, i, j + 1, memo), longestCommonSubsequence(str1, str2, i + 1, j, memo))
+    return recursion(m - 1, n - 1);
+    
+    function recursion(index1, index2) {
+        if (index1 < 0 || index2 < 0) return 0;
+
+        if (memo[index1][index2]) return memo[index1][index2];
+
+        let result;
+
+        if (text1[index1] === text2[index2]) {
+            result = recursion(index1 - 1, index2 - 1) + 1;
+        } else {
+            result = Math.max(recursion(index1, index2 - 1), recursion(index1 - 1, index2));
+        }
+
+        memo[index1][index2] = result;
+        return result;
+    }
 };
