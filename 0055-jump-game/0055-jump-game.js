85be29cab3f7c1cc211afa95cca14247753b64f5
Victor Zarour <victor.zarour@gmail.com>
2022-11-29 19:56:09 -0500

    Time: 128 ms (53.23%), Space: 46.6 MB (44.47%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -13,12 +13,13 @@
 //   return memo[i] = false
 // };
 
-var canJump = function(nums) {
-    let last = nums.length - 1;
-    for (let i = last - 1; i >= 0; i--) {
-        if (i + nums[i] >= last) {
-            last = i;
-        };
-    };
-    return last === 0;
-};
+const canJump = (nums) => {
+    let last = nums.length - 1
+    
+    for (let i = last - 1; i >= 0; i--){
+        if (i + nums[i] >= last) last = i
+    }
+    
+    return last === 0
+}
+
