a93959a01454d3bcb673e440aa6c21184b266b0a
Victor Zarour <victor.zarour@gmail.com>
2022-11-30 11:35:06 -0500

    Time: 1897 ms (11.21%), Space: 169.8 MB (6.23%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
@@ -2,22 +2,19 @@
  * @param {string} s
  * @return {number}
  */
-var longestPalindromeSubseq = function(s) {
+var longestPalindromeSubseq = function(str, start = 0, end = str.length - 1, memo = {}) {
+  const key = start + ',' + end
   
-    const memo = [...Array(s.length)].map(() => Array(s.length).fill(0));
-    
-    function run(i, j) {
-        if(i === j) return 1;
-        if(i > j) return 0;
-        if(memo[i][j]) return memo[i][j]
-        
-        let res;
-        
-        if(s[i] === s[j]) res = run(i+1, j-1) + 2;
-        else res = Math.max(run(i+1, j), run(i, j-1));
-		
-        memo[i][j] = res;
-        return res;
-    }
-    return run(0, s.length-1);
+  if (key in memo) return memo[key]
+  if (start === end) return 1;
+  if (start > end) return 0;
+  
+  if (str[start] === str[end]){
+    return memo[key] = 2 + longestPalindromeSubseq(str, start + 1, end - 1, memo)
+  }
+  else {
+    let right = longestPalindromeSubseq(str, start, end - 1, memo)
+    let left = longestPalindromeSubseq(str, start + 1, end, memo)
+    return memo[key] = Math.max(right, left)
+  }
 };
