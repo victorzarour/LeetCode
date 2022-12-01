cdbce82452f9524457af174609a2ab47313e16ee
Victor Zarour <victor.zarour@gmail.com>
2022-12-01 16:03:38 -0500

    Time: 80 ms (84.79%), Space: 44.5 MB (42.72%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const map = new Map()
+    
+    for (let num of nums){
+        map.set(num, map.get(num) + 1|| 1)    
+    }
+    
+    for (let [key, value] of map){
+        if (value > nums.length / 2) return key
+    }
+    
+};
