06007090e1d492f57ad392c966f1500be0bb0db1
Victor Zarour <victor.zarour@gmail.com>
2022-12-02 09:00:41 -0500

    Time: 102 ms (20.54%), Space: 42.3 MB (25.97%) - LeetHub

---
+++ 0163-missing-ranges/0163-missing-ranges.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} lower
+ * @param {number} upper
+ * @return {string[]}
+ */
+var findMissingRanges = function(nums, lower, upper) {
+    nums = [lower-1, ...nums, upper+1];
+    let result = [];
+    for (let i=1;i<nums.length;i++) {
+        if (nums[i]!=nums[i-1] && nums[i]-1!=nums[i-1]) {
+            let num1 = nums[i-1]+1, num2 = nums[i]-1;
+            result.push(num1 + (num1==num2?"":("->"+num2)));
+            
+        }
+    }
+    return result;
+};
