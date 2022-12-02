fc91f45fadf3cdea9fb52c827affd13f68b89c05
Victor Zarour <victor.zarour@gmail.com>
2022-12-02 11:47:12 -0500

    Time: 68 ms (93.27%), Space: 42.2 MB (77.07%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+    const brackets = {
+        "(":")",
+        "{":"}",
+        "[":"]"
+    }
+
+    
+    const stack = []
+    
+    for (let char of s){
+        if (brackets[char]) stack.push(brackets[char])
+        else if (stack.pop() !== char) return false
+    }
+    
+    return (stack.length === 0)
+};
