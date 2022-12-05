811b760bf24ad5d5ea494e0354cb9100ddabee58
Victor Zarour <victor.zarour@gmail.com>
2022-12-05 12:40:17 -0500

    Time: 115 ms (9.43%), Space: 43.5 MB (5.66%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfParentheses = function(s) {
+  const stack = [0]
+  
+  for (let char of s){
+    if (char === '(') stack.push(0)
+    if (char === ')') {
+      if (stack[stack.length - 1] === 0){
+        stack.pop()
+        stack.push(stack.pop() + 1)
+        console.log(stack)
+      } 
+      else stack.push((stack.pop() * 2) + stack.pop())
+    }
+  }
+  return stack.pop()
+};
