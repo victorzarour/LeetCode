1cab31d2b0d4a55a750043d122c4733828017ee2
Victor Zarour <victor.zarour@gmail.com>
2022-12-07 14:05:25 -0500

    Time: 70 ms (97.42%), Space: 43.8 MB (96.77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+// var isPalindrome = function(s) {
+//   reverseString = s.split("").reverse().join("").toLowerCase()
+//   return s.toLowerCase().replace(/[^0-9a-z]/gi, '')  == reverseString.replace(/[^0-9a-z]/gi, '')
+// };
+
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/gi,'')
+    let i = 0
+    let j = s.length - 1
+    
+    while (i <= j) {
+        if (s[i] !== s[j]) return false
+        i++
+        j--
+    }
+    return true
+}
