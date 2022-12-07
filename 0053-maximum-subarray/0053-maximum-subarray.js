c6705dcf4beaaf186abf9c446bbb595f9507218d
Victor Zarour <victor.zarour@gmail.com>
2022-12-07 12:11:27 -0500

    Time: 130 ms (59.24%), Space: 50.7 MB (27.43%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -16,14 +16,14 @@
 //     return max   
 // };
 
-var maxSubArray = function(nums) {
+const maxSubArray = (nums) => {
     let current = nums[0]
     let max = nums[0]
     
-    for (let i = 1; i < nums.length; i++){
-        let num = nums[i]
+    for (let i = 1; i < nums.length; i++) {
+        const num = nums[i]
         current = Math.max(num, current + num)
         max = Math.max(max, current)
     }
     return max
-};
+}
