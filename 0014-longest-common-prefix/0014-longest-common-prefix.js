439829d348d0ef0261bbdfdd3c3e17f0693e6f6e
Victor Zarour <victor.zarour@gmail.com>
2022-12-12 14:07:46 -0500

    Time: 68 ms (93.30%), Space: 42.3 MB (78.26%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    
+    let commonPrefix = ""
+ 
+    for (let i = 0; i < strs[0].length; i++){
+        const char = strs[0][i]
+            for (let j = 1; j < strs.length; j++){
+                if (strs[j][i] !== char) return commonPrefix
+            }
+        commonPrefix = commonPrefix + char
+    }
+    
+    return commonPrefix    
+    
+    
+
+    
+};
