711b87b977ba51b52c037fc275784572fea9eb03
Victor Zarour <victor.zarour@gmail.com>
2022-12-15 09:41:02 -0500

    Time: 84 ms (94.08%), Space: 47.8 MB (86.49%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -3,16 +3,16 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
-    const vowelss = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    const vowels = new Set(vowelss)
+    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    const set = new Set(vowels)
     let first = 0
     let last = s.length - 1
     const split = s.split("")
     
     while (first <= last){
-        if (!vowels.has(split[first])) first++
-        else if (!vowels.has(split[last])) last--
-        else if (vowels.has(split[first]) && vowels.has(split[last])){
+        if (!set.has(split[first])) first++
+        else if (!set.has(split[last])) last--
+        else if (set.has(split[first]) && set.has(split[last])){
             [split[first], split[last]] = [split[last], split[first]]
             first++
             last--
