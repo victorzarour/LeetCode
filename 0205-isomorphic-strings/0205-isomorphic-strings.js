8155e9787cbf8e138b6539666ee73465fbfcc13e
Victor Zarour <victor.zarour@gmail.com>
2022-12-16 14:07:54 -0500

    Time: 87 ms (84.65%), Space: 43.8 MB (46.08%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,16 +4,14 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    let map1 = new Map()
-    let map2 = new Map()
+    let mapS = new Map()
+    let mapT = new Map()
     
     for (let i = 0; i < s.length; i++) {
+        if (!mapS.get(s[i])) mapS.set(s[i], t[i])
+        if (!mapT.get(t[i])) mapT.set(t[i], s[i])
         
-        if (!map1.get(s[i])) map1.set(s[i], t[i])
-        if (!map2.get(t[i])) map2.set(t[i], s[i])
-        
-        if (map1.get(s[i]) !== t[i] || map2.get(t[i]) !== s[i]) return false
+        if (mapS.get(s[i]) !== t[i] || mapT.get(t[i]) !== s[i]) return false
     }
     return true
 };
-
