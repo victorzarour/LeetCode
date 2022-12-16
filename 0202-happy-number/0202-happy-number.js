4592115417a3c6664bcc6a5e3248eb367d92aa95
Victor Zarour <victor.zarour@gmail.com>
2022-12-16 14:33:11 -0500

    Time: 67 ms (95.35%), Space: 42.3 MB (91.58%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+function sq(num) {
+  let sum = 0;
+    
+  while (num > 0) {
+    let d = num % 10;
+    sum += d * d;
+    num = Math.floor(num/10);
+  }
+  return sum;
+}
+
+function isHappy(n) {
+    
+  let slow = fast = n;
+    
+  while (true) {
+    slow = sq(slow);
+    fast = sq(sq(fast));
+    if (slow === fast) break;
+  }
+  return slow === 1;
+}
