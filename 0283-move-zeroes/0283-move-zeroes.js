8c2ea1f049df1417ad6466466ad9366a4c03af33
Victor Zarour <victor.zarour@gmail.com>
2022-12-16 08:06:53 -0500

    Time: 86 ms (97.96%), Space: 46.7 MB (59.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,12 +2,24 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// var moveZeroes = function(nums) {
+//     for(let i= nums.length-1; i>=0; i--){
+//         if(nums[i]===0){
+//             nums.push(0) 
+//             nums.splice(i,1)
+//         }
+//     }
+//     return nums
+// };
+
 var moveZeroes = function(nums) {
-    for(let i= nums.length-1; i>=0; i--){
-        if(nums[i]===0){
-            nums.push(0) 
-            nums.splice(i,1)
+    let last = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            [nums[i], nums[last]] = [nums[last], nums[i]];
+            last++;
         }
     }
-    return nums
+	return nums;
 };
