e8d19f520fde0e5f3353717083f02997b31499b3
Victor Zarour <victor.zarour@gmail.com>
2022-12-18 11:47:09 -0500

    Time: 95 ms (86.62%), Space: 48 MB (68.40%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var validPalindrome = function(s) {
+    let i = 0
+    let j = s.length - 1
+    
+    while (i < j){
+        if (s[i] !== s[j]) {
+            return _validPalindrome(s, i, j - 1) || _validPalindrome(s, i + 1, j)
+        }
+        i++
+        j--
+    }
+    return true
+}
+
+var _validPalindrome = function(s, i, j) {
+    while (i < j) {
+        if (s[i] !== s[j]) return false
+        i++
+        j--
+    }
+    return true
+};
