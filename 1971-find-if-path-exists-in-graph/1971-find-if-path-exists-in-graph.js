fd6fefb0278ace2775f03e8e1e1bb3f2d009aa9f
Victor Zarour <victor.zarour@gmail.com>
2022-12-19 14:00:52 -0500

    Time: 706 ms (74.10%), Space: 154.7 MB (57.68%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -6,30 +6,32 @@
  * @return {boolean}
  */
 var validPath = function(n, edges, source, destination) {
-    const graph = {}
-
-    for (let [ a, b ] of edges){   
-        if (!graph[a]) graph[a] = []
-        if (!graph[b]) graph[b] = []
-
-        graph[a].push(b)
-        graph[b].push(a)
-    }
+    const graph = createGraph(edges)
     
     const stack = [ source ]
     const set = new Set()
-
+    
     while (stack.length){
         const current = stack.pop()
-        set.add(String(current))
+        set.add(current)
         if (current === destination) return true
-
-        for (let neighbor of graph[current]){
-          if(!set.has(String(neighbor))) stack.push(neighbor)
-        }
+        
+        for (let edge of graph[current]) {
+            if (!set.has(edge)) stack.push(edge)   
+        }       
     }
-
     return false
 };
 
-
+const createGraph = (edges) => {
+    const graph = {}
+    
+    for (let [ a, b ] of edges){
+        if (!graph[a]) graph[a] = []
+        if (!graph[b]) graph[b] = []
+        
+        graph[a].push(b)
+        graph[b].push(a)
+    }
+    return graph
+}
