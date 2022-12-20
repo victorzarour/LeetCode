5be7ee5ed6404ecb3f75bb5d937f802b5a0e8b48
Victor Zarour <victor.zarour@gmail.com>
2022-12-20 09:38:03 -0500

    Time: 72 ms (84.52%), Space: 44.5 MB (37.50%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {
+    const queue = [0]
+    const set = new Set()
+    set.add(0)  
+
+    while (queue.length){
+        const current = queue.shift()
+        set.add(current)
+
+        for (let key of rooms[current]){
+            if (!set.has(key)) queue.push(key)
+        }        
+    }
+    
+    console.log(set)
+    return set.size === rooms.length
+
+    
+    
+    
+};
