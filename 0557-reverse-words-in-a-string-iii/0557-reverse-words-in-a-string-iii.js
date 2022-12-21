deec20a4c6f37b42cfc316cb2a6e1122642eedca
Victor Zarour <victor.zarour@gmail.com>
2022-12-21 10:45:08 -0500

    Time: 166 ms (6.10%), Space: 48.8 MB (24.55%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -3,5 +3,19 @@
  * @return {string}
  */
 var reverseWords = function(s) { 
-    return s.split(" ").map(word => word.split("").reverse().join("")).join(" ")
+    // return s.split(" ").map(word => word.split("").reverse().join("")).join(" ")
+    
+    split = s.split(" ")
+    
+    for (let i = 0; i < split.length; i++) {
+        let reversed = ""
+        let wordLength = split[i].length - 1
+
+        while (wordLength >= 0){
+            reversed += split[i][wordLength];
+            wordLength--
+        }
+        split[i] = reversed
+    }
+    return split.join(" ")
 };
