e3171bf98590c983589ba08e7c0af24b84130890
Victor Zarour <victor.zarour@gmail.com>
2022-12-22 09:26:16 -0500

    Time: 66 ms (91.78%), Space: 43.6 MB (46.47%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    const set1 = new Set()
+    const set1 = new Set(nums1)
     const finalSet = new Set()
     
-    for (let num of nums1) {
-        set1.add(num)
-    }
+    // for (let num of nums1) {
+    //     set1.add(num)
+    // }
     
     for (let num of nums2) {
         if (set1.has(num)) finalSet.add(num)
