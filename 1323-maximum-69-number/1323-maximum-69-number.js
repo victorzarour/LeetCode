2a5db29cdd41f724f5540bdbb084f0d45da61d7d
Victor Zarour <victor.zarour@gmail.com>
2022-12-23 11:11:22 -0500

    Time: 90 ms (63.65%), Space: 42.3 MB (22.52%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.js
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var maximum69Number  = function(num) {
-    const nums = myArr = String(num).split("").map((num)=> Number(num))
+    const nums = String(num).split("").map((num)=> Number(num))
     
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === 6) {
