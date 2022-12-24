6dc60e78c2e18e6985aa60ebc3f5b63965c71125
Victor Zarour <victor.zarour@gmail.com>
2022-12-24 12:36:41 -0500

    Time: 66 ms (98.63%), Space: 45.1 MB (73.35%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.js
+++ 1002-find-common-characters/1002-find-common-characters.js
@@ -6,9 +6,7 @@ var commonChars = function(words) {
     let common = words[0].split('').filter(char => {
         for (let i = 1; i < words.length; i++) {
             if (!words[i].includes(char)) return;
-            else {
-                words[i] = words[i].replace(char, '')
-            }
+            else words[i] = words[i].replace(char, '')
         }
         return true;
     })
