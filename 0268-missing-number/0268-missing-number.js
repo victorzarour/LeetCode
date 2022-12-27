2958031324e1f11d5d198ad57b4b528f21af0e8c
Victor Zarour <victor.zarour@gmail.com>
2022-12-27 09:59:03 -0500

    Time: 108 ms (55.19%), Space: 43.8 MB (97.03%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -12,13 +12,8 @@ var missingNumber = function(nums) {
     
     nums = nums.sort((a,b) => a-b)
     
-            if (nums[nums.length-1] != nums.length) {
-            return nums.length;
-        }
-        // Ensure that 0 is at the first index
-        else if (nums[0] != 0) {
-            return 0;
-        }
+    if (nums[nums.length - 1] != nums.length) return nums.length;
+    else if (nums[0] != 0) return 0;
     
     for (let i = 1; i < nums.length; i++){
         const num = nums[i - 1] + 1;
