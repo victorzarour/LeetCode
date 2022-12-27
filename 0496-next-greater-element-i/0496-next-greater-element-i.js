a3db0786386c80cb1d4d3e36f731890b1e88697e
Victor Zarour <victor.zarour@gmail.com>
2022-12-27 09:14:52 -0500

    Time: 70 ms (91.82%), Space: 43.3 MB (82.56%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    const map = new Map()
+    
+    for (let i = 0; i < nums2.length; i++){
+        let j = i + 1
+        while (nums2[j] <= nums2[i]){
+            j++
+        }       
+        if (j === nums2.length) j = -1
+        map.set(nums2[i], nums2[j] || j)
+    }
+    
+    for (let i = 0; i < nums1.length; i++){
+        nums1[i] = map.get(nums1[i])
+    }
+    
+    return nums1
+};
