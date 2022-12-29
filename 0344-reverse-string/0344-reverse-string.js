4ceb4cec65c95d745c0e57d5ab247bbe32f0a10a
Victor Zarour <victor.zarour@gmail.com>
2022-12-29 11:17:03 -0500

    Time: 86 ms (97.90%), Space: 48.5 MB (97.09%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,7 +3,7 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    for (let i = 0, last = s.length - 1; i < last; i++, last--){
-        [s[i], s[last]] = [s[last], s[i]]
+    for (let i = 0, l = s.length - 1; i < l; i++, l--){
+        [s[i], s[l]] = [s[l], s[i]]
     }
 };
