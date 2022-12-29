32a634754aeecfe47de020f4fc1811acff1ab0c5
Victor Zarour <victor.zarour@gmail.com>
2022-12-29 11:33:40 -0500

    Time: 137 ms (58.82%), Space: 53 MB (8.82%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.js
+++ 0697-degree-of-an-array/0697-degree-of-an-array.js
@@ -6,20 +6,17 @@ var findShortestSubArray = function(nums) {
     let obj = {}, max = 0, result = nums.length;
     
     for (let i = 0; i < nums.length; i++) {
-        if (!obj[nums[i]]) {
-            obj[nums[i]] = [i];
-        } else {
-            obj[nums[i]].push(i);
-        }
+        if (!obj[nums[i]]) obj[nums[i]] = [i];
+        else obj[nums[i]].push(i);
     }
     
     console.log(obj)
     
     for (let [key, value] of Object.entries(obj)) {
-        if(value.length === max) {
+        if (value.length === max) {
             result = Math.min(result, value[value.length - 1] - value[0] + 1);
         }
-        if(value.length > max) {
+        if (value.length > max) {
             result = value[value.length - 1] - value[0] + 1;
             max = value.length;
         }
