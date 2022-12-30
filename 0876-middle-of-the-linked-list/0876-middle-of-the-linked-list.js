2b38d208a38058ec19bcfe4a0e6d437cf7c65fa3
Victor Zarour <victor.zarour@gmail.com>
2022-12-30 08:53:38 -0500

    Time: 61 ms (90.86%), Space: 41.8 MB (74.10%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -9,14 +9,26 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var middleNode = function(head) {
-    const array = []
-    let current = head
+// var middleNode = function(head) {
+//     const array = []
+//     let current = head
     
-    while (current){
-        array.push(current)
-        current = current.next
+//     while (current){
+//         array.push(current)
+//         current = current.next
+//     }
+    
+//     return array[Math.floor(array.length / 2)]
+// };
+
+var middleNode = function(head) {
+    let fast = head;
+    let slow = head;
+    
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
     
-    return array[Math.floor(array.length / 2)]
-};
+    return slow;
+}
