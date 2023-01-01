d918b6c0de18893f37c99a646b6e0a362efd4c87
Victor Zarour <victor.zarour@gmail.com>
2023-01-01 09:56:49 -0500

    Time: 111 ms (69.25%), Space: 53.2 MB (5.17%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -5,12 +5,14 @@
 var findErrorNums = function(nums) {
     let duplicate, missing
     
-    const map = new Map()
+    const map = new Set()
     
     for (let num of nums) {
-        if (!map.has(num)) map.set(num, 1)
+        if (!map.has(num)) map.add(num)
         else duplicate = num
     }
+    
+    console.log(map)
     
     for (let i = 1; i <= nums.length; i++) {
         if (!map.has(i)) missing = i
