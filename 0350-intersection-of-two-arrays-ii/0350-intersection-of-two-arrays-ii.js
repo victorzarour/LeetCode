21d7737594f960f2e59a30cec4fa225497286f94
Victor Zarour <victor.zarour@gmail.com>
2023-01-02 12:13:49 -0500

    Time: 90 ms (60.12%), Space: 45.6 MB (6.88%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    const map1 = new Map()
+    const map2 = new Map()
+    const array = []
+    
+    for (let num of nums1) {
+        map1.set(num, map1.get(num) + 1 || 1)
+    }
+    for (let num of nums2) {
+        map2.set(num, map2.get(num) + 1 || 1)
+    }
+    for (let [ key, value ] of map1) {
+        if (map2.get(key)) {
+            for (let i = 0; i < Math.min(map1.get(key), map2.get(key)); i++) {
+                array.push(key)
+            }
+        }
+    }
+    
+    console.log(map1)
+    console.log(map2)
+    return array
+};
