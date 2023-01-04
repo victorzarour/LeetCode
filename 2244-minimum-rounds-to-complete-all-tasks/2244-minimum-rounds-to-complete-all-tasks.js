1dd110825e112cdc462a0f3c1fabf1a5b225f111
Victor Zarour <victor.zarour@gmail.com>
2023-01-04 10:08:12 -0500

    Time: 245 ms (62.74%), Space: 62.2 MB (49.02%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
@@ -12,18 +12,12 @@ var minimumRounds = function(tasks) {
     
     for (let [ key, value ] of map) {
         if (value < 2) return -1
-        else if (value % 3 == 0) {
-            count += (value / 3)
-            map.delete(key)
-        }
+        else if (value % 3 == 0) count += (value / 3)
         else if (value % 3 == 1) {
             let number = (value - 1) / 3
             count += ((number - 1) + 2)
         }
-        else if (value % 3 == 2) {
-            count += (((value - 2) / 3) + 1)
-        }
-        
+        else if (value % 3 == 2) count += ((value - 2) / 3) + 1      
     }
     return count
 };
