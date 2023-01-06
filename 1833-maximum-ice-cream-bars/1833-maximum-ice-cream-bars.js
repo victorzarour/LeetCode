dca1e9dc9505d7b0e4ea5f3d247ed785fa13a8f9
Victor Zarour <victor.zarour@gmail.com>
2023-01-06 10:18:45 -0500

    Time: 486 ms (31.67%), Space: 55.8 MB (56.67%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -3,30 +3,13 @@
  * @param {number} coins
  * @return {number}
  */
-// var maxIceCream = function(costs, coins) {
-//     costs = costs.sort((a, b) => a-b)
-//     let max = 0
-//     for (let cost of costs) {
-//         if (coins < cost) break
-//         max++
-//         coins -= cost
-//     }
-//     return max
-// };
-
 var maxIceCream = function(costs, coins) {
-    // Store ice cream costs in increasing order.
-    costs.sort((num1, num2) => num1 - num2);
-
-    let n = costs.length;
-    let icecream = 0;
-
-    // Pick ice creams till we can.
-    while (icecream < n && costs[icecream] <= coins) {
-        // We can buy this icecream, reduce the cost from the coins. 
-        coins -= costs[icecream];
-        icecream += 1;
+    costs = costs.sort((a, b) => a-b)
+    let max = 0
+    for (let cost of costs) {
+        if (coins < cost) break
+        max++
+        coins -= cost
     }
-
-    return icecream;
+    return max
 };
