4207598d401faf2dfb97ff2f9ad84f921d7260d8
Victor Zarour <victor.zarour@gmail.com>
2023-01-06 10:59:52 -0500

    Time: 110 ms (5.97%), Space: 42 MB (55.82%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.js
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -7,7 +7,7 @@ var summaryRanges = function(nums) {
 
     for (let i = 0, left = nums[0]; i < nums.length; i++) {
         if (nums[i] + 1 !== nums[i + 1]) {
-          array.push(left === nums[i] ? '' + nums[i] : left + '->' + nums[i]);
+          array.push(left === nums[i] ? `${nums[i]}` : `${left}->${nums[i]}`);
           left = nums[i + 1];
         }
     }
