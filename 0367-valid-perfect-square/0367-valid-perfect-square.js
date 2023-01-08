c4165384e46e07c8aab12bf5809e60fe69aa3cf6
Victor Zarour <victor.zarour@gmail.com>
2023-01-08 15:02:40 -0500

    Time: 53 ms (98.59%), Space: 42.1 MB (26.84%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    for (let i = 1; i * i <= num; i++) {
+        if (i * i === num) return true 
+    }
+    return false
+};
