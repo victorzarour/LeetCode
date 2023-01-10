80dd3a5c532b977d1fa09f6f10ec173a76fdf698
Victor Zarour <victor.zarour@gmail.com>
2023-01-10 07:48:29 -0500

    Time: 99 ms (83.91%), Space: 44 MB (97.00%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -3,19 +3,29 @@
  * @param {number[]} s
  * @return {number}
  */
-var findContentChildren = function(g, s) {
-    let count = 0
-    g = g.sort((a, b) => a-b)
-    s = s.sort((a, b) => a-b)
+// var findContentChildren = function(g, s) {
+//     let count = 0
+//     g = g.sort((a, b) => a - b)
+//     s = s.sort((a, b) => a - b)
     
-    for (let i = 0; i < g.length; i++) {
-        for (let j = 0; j < s.length; j++) {
-            if (s[j] >= g[i]) {
-                count++
-                s.splice(j, 1)
-                break
-            }
-        }
-    }
-    return count
+//     for (let i = 0; i < g.length; i++) {
+//         for (let j = 0; j < s.length; j++) {
+//             if (s[j] >= g[i]) {
+//                 count++
+//                 s.splice(j, 1)
+//                 break
+//             }
+//         }
+//     }
+//     return count
+// };
+
+const findContentChildren = (g, s) => {
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+    let gi = 0
+    for (let si = 0; gi < g.length && si < s.length; ++si)
+        if (g[gi] <= s[si]) ++gi;
+    return gi;
 };
+
