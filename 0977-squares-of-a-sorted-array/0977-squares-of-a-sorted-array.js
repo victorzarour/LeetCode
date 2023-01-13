9a93ba0ed317cd232f8447dd513620f424b5b5d9
Victor Zarour <victor.zarour@gmail.com>
2023-01-13 10:59:31 -0500

    Time: 121 ms (62.56%), Space: 48.2 MB (82.99%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -2,12 +2,21 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-    const squares = []
+// var sortedSquares = function(nums) {
+//     const squares = []
     
-    for (let num of nums) {
-        squares.push(num * num)
+//     for (let num of nums) {
+//         squares.push(num * num)
+//     }
+    
+//     return squares.sort((a,b) => a-b)
+// };
+
+var sortedSquares = function(nums) {
+    
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = nums[i] * nums[i]
     }
     
-    return squares.sort((a,b) => a-b)
+    return nums.sort((a,b) => a-b)
 };
