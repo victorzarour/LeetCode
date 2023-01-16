b8c3b07df1f0f885bfa25318796b9af64bf797bb
Victor Zarour <victor.zarour@gmail.com>
2023-01-16 09:56:06 -0600

    Time: 69 ms (75.22%), Space: 41.9 MB (92.19%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.js
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -3,13 +3,23 @@
  * @param {string} stones
  * @return {number}
  */
+// var numJewelsInStones = function(jewels, stones) {
+//     let count = 0
+    
+//     for (let jewel of jewels) {
+//         for (let stone of stones) {
+//             if (jewel === stone) count++
+//         }
+//     }
+//     return count
+// };
+
 var numJewelsInStones = function(jewels, stones) {
+    const set = new Set(jewels)
     let count = 0
     
-    for (let jewel of jewels) {
-        for (let stone of stones) {
-            if (jewel === stone) count++
-        }
+    for (let stone of stones) {
+        if (set.has(stone)) count++
     }
     return count
 };
