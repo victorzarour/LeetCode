b8da7fbc9bd18e59beb6708e7c79e59fda09bfa8
Victor Zarour <victor.zarour@gmail.com>
2023-01-19 12:15:57 -0600

    Time: 65 ms (81.79%), Space: 42 MB (88.35%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -3,43 +3,11 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    const alphabet = [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-    ];
-    const map = new Map()
-    for (let letter of alphabet) {
-        map.set(letter.toLowerCase(), 0)
-    }
-    for (let char of sentence) {
-        map.set(char, map.get(char) + 1)
-    }
-    for (let val of map.values()) {
-        if (val === 0) return false
-    }
-    return true
+   const set = new Set()
+   
+   for (let char of sentence) {
+       set.add(char)
+   }
+   
+   return set.size === 26
 };
