c629efe11ad1430cd46583ab0346f4fe655be7ea
Victor Zarour <victor.zarour@gmail.com>
2023-01-20 10:45:10 -0600

    Time: 159 ms (84.85%), Space: 57.8 MB (87.88%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var findSubsequences = function(nums) {
+    if (nums.length < 2) return [];
+    let result = [], path = [];
+    const backtrack = (startIndex) => {
+        if(path.length >= 2) {
+            result.push([...path]);
+        }
+        var used = new Set(); // Important, we need to check whether the num has been used on this level to avoid duplicate
+        for(var i = startIndex; i < nums.length; i++) {
+            if(path.length > 0 && nums[i] < path[path.length - 1] || used.has(nums[i])) continue;
+            used.add(nums[i]);
+            path.push(nums[i]);
+            backtrack(i + 1);
+            path.pop();
+        }
+        return;
+    }
+    backtrack(0);
+    return result;
+};
