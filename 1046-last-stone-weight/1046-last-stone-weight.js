128bc740309575a18c0813e91a647ac986e91da1
Victor Zarour <victor.zarour@gmail.com>
2023-01-22 08:42:22 -0600

    Time: 119 ms (10.68%), Space: 44.3 MB (11.74%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -5,7 +5,10 @@
 var lastStoneWeight = function(stones) {
     while (stones.length > 1) {
         stones = stones.sort((a, b) => b - a)
-        if (stones[0] === stones[1]) stones.splice(0, 2)
+        if (stones[0] === stones[1]){
+            stones.shift()
+            stones.shift()            
+        } 
         else {
             stones[0] = stones[0] - stones[1] 
             stones.splice(1, 1)
