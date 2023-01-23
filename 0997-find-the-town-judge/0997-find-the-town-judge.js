605caa281b0f54d878b1adb170211d952464bffd
Victor Zarour <victor.zarour@gmail.com>
2023-01-23 11:53:08 -0600

    Time: 218 ms (11.16%), Space: 52.2 MB (14.40%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.js
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -5,14 +5,20 @@
  */
 var findJudge = function(n, trust) {
     const counts = new Array(n + 1).fill(0);
+    const map = new Map()
+    
+    console.log(counts)
      
-    for(let [ i , j ] of trust) {
+    for (let [ i , j ] of trust) {
         counts[i] -= 1
         counts[j] += 1
     }
+    
+    console.log(counts)
 
     for (let i = 1; i < counts.length; i++) {
         if (n - 1 === counts[i]) return i
     }
     return -1
 };
+
