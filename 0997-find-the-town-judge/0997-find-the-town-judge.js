c3bacd222c80dfe28ebda68cd75ff5042abdefda
Victor Zarour <victor.zarour@gmail.com>
2023-01-23 10:51:49 -0600

    Time: 112 ms (66.13%), Space: 50.6 MB (60.65%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.js
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -4,30 +4,26 @@
  * @return {number}
  */
 var findJudge = function(N, trust) {
-  let peeps = new Map();  // each person (up to N) is a key in a map
-  for (let i = 1; i <= N; i++) {
-    peeps.set(i, 0);
-  }
-
-  for (let i = 0; i < trust.length; i++) {
-  	peeps.delete(trust[i][0]); // if the person trusts someone, delete the person from map
-  }
-
-  if (peeps.size === 0) {
-  		return -1; // if map is empty, return -1
-  } else {
-  	let judge = peeps.keys().next().value; // get the only key remaining in the map
-  	let count = 0; // used to count how many times the judge was trusted
-  	for (let i = 0; i < trust.length; i++) {
-  		if (trust[i][1] === judge) { // if judge is found in the array, increment the map value
-  			count += 1;
-  		}
-  	}
-  	if (count == (N-1)) {
-  		return judge; // # of needed trusts for the judge must be N-1 (judge can't trust judge)
-  	} else {
-  		return -1;
-  	}
-  }
+    // Initialize array with 1..N
+    // N + 1, so count is easier 
+    const counts = new Array(N+1).fill(0);
     
+    // Iterate through N people    
+    for(let [i,j] of trust) {
+        // If the person trusts somebody, then it does not meet requirement 1
+        counts[i] -= 1
+        
+        // Count the people they trust
+        counts[j] += 1
+    }
+
+    // If N-1 exists, then its the judge since the judge trusts no one except themselves
+    // and everyone else trusts the judge, then its N-1
+    for(let i = 1; i < counts.length; i++) {
+        if ((N-1) === counts[i]) {
+            return i;
+        }
+    }
+    
+    return -1
 };
