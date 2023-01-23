4c32f017cccf8b716fb5e969337536eec6419cd9
Victor Zarour <victor.zarour@gmail.com>
2023-01-23 10:51:39 -0600

    Time: 105 ms (77.89%), Space: 50.9 MB (45.44%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.js
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -4,24 +4,30 @@
  * @return {number}
  */
 var findJudge = function(N, trust) {
-    const truster = Array(N).fill(0); // keep track of count of people this person trusts
-    const trustee = Array(N).fill(0); // keep track of count of people that trust this person
-    
-    for(let i = 0; i < trust.length; i++) {
-        let [a, b] = trust[i];
-        a--; //get indexes correct
-        b--;
-        
-        truster[a]++;
-        trustee[b]++;
-    }
-    
-    for(let i = 0; i < N; i++) {
-        if (truster[i] == 0 && trustee[i] == N - 1) {
-            return i + 1;
-        }
-    }
-    
-    return -1;
+  let peeps = new Map();  // each person (up to N) is a key in a map
+  for (let i = 1; i <= N; i++) {
+    peeps.set(i, 0);
+  }
 
+  for (let i = 0; i < trust.length; i++) {
+  	peeps.delete(trust[i][0]); // if the person trusts someone, delete the person from map
+  }
+
+  if (peeps.size === 0) {
+  		return -1; // if map is empty, return -1
+  } else {
+  	let judge = peeps.keys().next().value; // get the only key remaining in the map
+  	let count = 0; // used to count how many times the judge was trusted
+  	for (let i = 0; i < trust.length; i++) {
+  		if (trust[i][1] === judge) { // if judge is found in the array, increment the map value
+  			count += 1;
+  		}
+  	}
+  	if (count == (N-1)) {
+  		return judge; // # of needed trusts for the judge must be N-1 (judge can't trust judge)
+  	} else {
+  		return -1;
+  	}
+  }
+    
 };
