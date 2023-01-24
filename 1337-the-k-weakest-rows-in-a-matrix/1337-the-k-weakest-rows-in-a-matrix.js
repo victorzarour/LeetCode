95f4a7c88cb155dc5f53be2840f9e40232c3a72c
Victor Zarour <victor.zarour@gmail.com>
2023-01-24 09:17:45 -0600

    Time: 72 ms (71.23%), Space: 44.6 MB (19.84%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -13,17 +13,17 @@ var kWeakestRows = function(mat, k) {
         }
     }
 
-    const mapSort = new Map([...map.entries()].sort((a, b) => a[1] - b[1]));
+//     const mapSort = new Map([...map.entries()].sort((a, b) => a[1] - b[1]));
    
-    const array = []
+//     const array = []
     
-    for (let [ key, value ] of mapSort) {
-        array.push(key)
-    }
+//     for (let [ key, value ] of mapSort) {
+//         array.push(key)
+//     }
      
-    return array.slice(0, k)
+//     return array.slice(0, k)
     
-        // return [...map.keys()]
-        // .sort((a,b) => map.get(a) !== map.get(b) ? map.get(a) - map.get(b) : a - b)
-        // .slice(0, k);
+        return [...map.keys()]
+        .sort((a,b) => map.get(a) !== map.get(b) ? map.get(a) - map.get(b) : a - b)
+        .slice(0, k);
 };
