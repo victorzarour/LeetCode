a7c6e965b02ebb31b822f3efcace68670bad4a4a
Victor Zarour <victor.zarour@gmail.com>
2023-01-24 09:08:57 -0600

    Time: 105 ms (25.60%), Space: 44.7 MB (16.57%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+// var kWeakestRows = function(mat, k) {
+//     const map = new Map()
+    
+//     for (let row = 0; row < mat.length; row++) {
+//         for (let person of mat[row]) {
+//             if (person === 1) map.set(row, map.get(row) + 1 || 1)
+//         }
+//     }
+    
+// //     const mapSort = new Map([...map.entries()].sort((a, b) => a[1] - b[1]));
+   
+// //     const array = []
+    
+// //     for (let [ key, value ] of mapSort) {
+// //         array.push(key)
+// //     }
+    
+// //     const index = mat.length % 2 === 1 ? Math.floor(mat.length/2 + 1) : Math.floor(mat.length/2)
+    
+// //     return array.slice(0, index)
+    
+//         return [...map.keys()]
+//         .sort((a,b) => map.get(a) !== map.get(b) ? map.get(a) - map.get(b) : a - b)
+//         .slice(0, k);
+// };
+
+
+var kWeakestRows = function(mat, k) {
+    const map = new Map();
+    for(const [i, m] of mat.entries()) {
+        map.set(i, m.reduce((a, b) => a + b));
+    }
+    return [...map.keys()]
+        .sort((a,b) => map.get(a) !== map.get(b) ? map.get(a) - map.get(b) : a - b)
+        .slice(0, k);
+};
