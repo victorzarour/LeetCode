87f264fa5cba69da4743c9e6bff3d5d71a2310f2
Victor Zarour <victor.zarour@gmail.com>
2023-01-24 09:15:40 -0600

    Time: 120 ms (10.52%), Space: 45.4 MB (9.92%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -13,31 +13,17 @@ var kWeakestRows = function(mat, k) {
         }
     }
     console.log(map)
-//     const mapSort = new Map([...map.entries()].sort((a, b) => a[1] - b[1]));
+    const mapSort = new Map([...map.entries()].sort((a, b) => a[1] - b[1]));
    
-//     const array = []
+    const array = []
     
-//     for (let [ key, value ] of mapSort) {
-//         array.push(key)
-//     }
+    for (let [ key, value ] of mapSort) {
+        array.push(key)
+    }
+     
+    return array.slice(0, k)
     
-//     const index = mat.length % 2 === 1 ? Math.floor(mat.length/2 + 1) : Math.floor(mat.length/2)
-    
-//     return array.slice(0, index)
-    
-        return [...map.keys()]
-        .sort((a,b) => map.get(a) !== map.get(b) ? map.get(a) - map.get(b) : a - b)
-        .slice(0, k);
+        // return [...map.keys()]
+        // .sort((a,b) => map.get(a) !== map.get(b) ? map.get(a) - map.get(b) : a - b)
+        // .slice(0, k);
 };
-
-
-// var kWeakestRows = function(mat, k) {
-//     const map = new Map();
-//     for(const [i, m] of mat.entries()) {
-//         map.set(i, m.reduce((a, b) => a + b));
-//     }
-//     console.log(map)
-//     return [...map.keys()]
-//         .sort((a,b) => map.get(a) !== map.get(b) ? map.get(a) - map.get(b) : a - b)
-//         .slice(0, k);
-// };
