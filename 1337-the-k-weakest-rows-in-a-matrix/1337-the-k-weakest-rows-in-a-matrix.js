d4d307c5dbb7743fb03d915a2a9f030cd89cfb1f
Victor Zarour <victor.zarour@gmail.com>
2023-01-24 09:15:12 -0600

    Time: 63 ms (92.86%), Space: 44.1 MB (61.81%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -3,39 +3,41 @@
  * @param {number} k
  * @return {number[]}
  */
-// var kWeakestRows = function(mat, k) {
-//     const map = new Map()
+var kWeakestRows = function(mat, k) {
+    const map = new Map()
     
-//     for (let row = 0; row < mat.length; row++) {
-//         for (let person of mat[row]) {
-//             if (person === 1) map.set(row, map.get(row) + 1 || 1)
-//         }
+    for (let row = 0; row < mat.length; row++) {
+        map.set(row, 0)
+        for (let person of mat[row]) {
+            if (person === 1) map.set(row, map.get(row) + 1 || 1)
+        }
+    }
+    console.log(map)
+//     const mapSort = new Map([...map.entries()].sort((a, b) => a[1] - b[1]));
+   
+//     const array = []
+    
+//     for (let [ key, value ] of mapSort) {
+//         array.push(key)
 //     }
     
-// //     const mapSort = new Map([...map.entries()].sort((a, b) => a[1] - b[1]));
-   
-// //     const array = []
+//     const index = mat.length % 2 === 1 ? Math.floor(mat.length/2 + 1) : Math.floor(mat.length/2)
     
-// //     for (let [ key, value ] of mapSort) {
-// //         array.push(key)
-// //     }
+//     return array.slice(0, index)
     
-// //     const index = mat.length % 2 === 1 ? Math.floor(mat.length/2 + 1) : Math.floor(mat.length/2)
-    
-// //     return array.slice(0, index)
-    
-//         return [...map.keys()]
-//         .sort((a,b) => map.get(a) !== map.get(b) ? map.get(a) - map.get(b) : a - b)
-//         .slice(0, k);
-// };
-
-
-var kWeakestRows = function(mat, k) {
-    const map = new Map();
-    for(const [i, m] of mat.entries()) {
-        map.set(i, m.reduce((a, b) => a + b));
-    }
-    return [...map.keys()]
+        return [...map.keys()]
         .sort((a,b) => map.get(a) !== map.get(b) ? map.get(a) - map.get(b) : a - b)
         .slice(0, k);
 };
+
+
+// var kWeakestRows = function(mat, k) {
+//     const map = new Map();
+//     for(const [i, m] of mat.entries()) {
+//         map.set(i, m.reduce((a, b) => a + b));
+//     }
+//     console.log(map)
+//     return [...map.keys()]
+//         .sort((a,b) => map.get(a) !== map.get(b) ? map.get(a) - map.get(b) : a - b)
+//         .slice(0, k);
+// };
