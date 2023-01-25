0d217553211af2344c02f32c43166834cfd3bb6f
Victor Zarour <victor.zarour@gmail.com>
2023-01-25 10:36:05 -0600

    Time: 80 ms (37.46%), Space: 41.6 MB (92.65%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let steps = 0
+    
+    while (num >= 1) {
+        if (num % 2 === 0) num /= 2
+        else num -= 1
+        steps++
+    }
+    
+    return steps
+};
