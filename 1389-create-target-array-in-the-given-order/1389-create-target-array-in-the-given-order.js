b3e4d7711c14632a79a5b6b8082d19efb598b7c3
Victor Zarour <victor.zarour@gmail.com>
2023-01-26 13:20:32 -0600

    Time: 68 ms (58.35%), Space: 42.3 MB (21.11%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
@@ -4,15 +4,9 @@
  * @return {number[]}
  */
 var createTargetArray = function(nums, index) {
-    // const target = []
-    // for (let i = 0; i < nums.length; i++) {
-    //     target.splice(index[i], 0, nums[i])
-    // }    
-    // return target
-    
-        return index.reduce((result, index, i) => {
-        const leftSide = result.slice(0, index);
-        const rightSide = result.slice(index);
-        return [...leftSide, nums[i], ...rightSide];
-    }, []);
+    const target = []
+    for (let i = 0; i < nums.length; i++) {
+        target.splice(index[i], 0, nums[i])
+    }    
+    return target
 };
