482468c5e3e1705dbad1aa0e6c9f151448ababfe
Victor Zarour <victor.zarour@gmail.com>
2023-01-26 12:34:24 -0600

    Time: 85 ms (39.63%), Space: 44.4 MB (25.07%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.js
+++ 0832-flipping-an-image/0832-flipping-an-image.js
@@ -5,19 +5,14 @@
 var flipAndInvertImage = function(image) {
     for (let row of image) {
         for (let start = 0, end = row.length - 1; start <= end; start++) {
-            if (start === end) {
-                row[start] = row[start] === 0 ? 1 : 0
-            } else {
+            if (start === end) row[start] = row[start] === 0 ? 1 : 0
+            else {
                 [row[start], row[end]] = [row[end], row[start]]
                 row[start] = row[start] === 0 ? 1 : 0
                 row[end] = row[end] === 0 ? 1 : 0           
             }
             end--
         }
-        // for (let i = 0; i < row.length; i++) {
-        //     if (row[i] === 0) row[i] = 1
-        //     else if (row[i] === 1) row[i] = 0
-        // }
     }
     return image
 };
