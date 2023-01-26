51fc18963badc58758a029d4cb4780f3f9e021f5
Victor Zarour <victor.zarour@gmail.com>
2023-01-26 13:17:52 -0600

    Time: 74 ms (44.51%), Space: 42.2 MB (32.10%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function(nums, index) {
+    const target = []
+    
+    for (let i = 0; i < nums.length; i++) {
+        target.splice(index[i], 0, nums[i])
+    }    
+    return target
+};
+
+// const months = ['Jan', 'March', 'April', 'June'];
+// months.splice(1, 0, 'Feb');
+// // Inserts at index 1
+// console.log(months);
+// // Expected output: Array ["Jan", "Feb", "March", "April", "June"]
