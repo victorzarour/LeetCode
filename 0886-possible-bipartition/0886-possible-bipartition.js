b1599c2a81a9b2d43400896629ae47877a34a0b2
Victor Zarour <victor.zarour@gmail.com>
2023-01-27 14:42:49 -0600

    Time: 277 ms (30.33%), Space: 57 MB (34.48%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.js
+++ 0886-possible-bipartition/0886-possible-bipartition.js
@@ -4,52 +4,33 @@
  * @return {boolean}
  */
 var possibleBipartition = function(n, dislikes) {
-    if (!dislikes.length) return true
-    
-    const graph = []
-    let color = new Array(n + 1).fill(-1);
-    
-    for(let i = 1; i < n + 1; i++){
-        graph[i] = [];
-    }
-    
-    for (let [ a, b ] of dislikes){     
-        graph[a].push(b)
+    const graph = {}
+
+    for (let [ a, b ] of dislikes){
+        if (!(a in graph)) graph[a] = []
+        if (!(b in graph)) graph[b] = []
+        graph[a].push(b) 
         graph[b].push(a)
     }
     
-    for(let i = 1; i <= n; i++){
-        if(color[i] == -1){
-            if(!isBipartite(graph,color,i)){
-                return false;
-            }
-        }
-    }
-    
-    console.log(graph)
-    
-    return true;
+    return canColor(graph)
 };
 
-var isBipartite = function(adj, color, curr){
-    
-    const queue = [curr];
-    color[curr] = 1;
-    let curColor = null;
-    
-    while(queue.length){
-        const current = queue.shift();
-        curColor = color[current]; 
-        
-        for (let i = 0; i < adj[current].length; i++){
-            
-            const v = adj[current][i];
-            
-            if (color[v] == -1){
-                color[v] = 1 - curColor;
-                queue.push(v);
-            } else if (color[v] == curColor) return false;
-        }
-    } 
-    return true;
+const canColor = (graph) => {
+  const coloring = {}
+  
+  for (let node in graph) {
+    if (!(node in coloring) && !validate(graph, node, coloring, false)) return false
+  }
+  return true
+};
+
+const validate = (graph, node, coloring, currentColor) => {
+  if (node in coloring) return currentColor === coloring[node];
+  coloring[node] = currentColor
+  
+  for (let neighbor of graph[node]) {
+    if (!validate(graph, neighbor, coloring, !currentColor)) return false
+  }
+  return true
 }
