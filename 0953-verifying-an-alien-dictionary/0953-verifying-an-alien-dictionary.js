abb29ca5178789535b7873ecacc94f6594326238
Victor Zarour <victor.zarour@gmail.com>
2023-01-28 15:40:12 -0600

    Time: 59 ms (94.43%), Space: 42.3 MB (74.94%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
@@ -6,7 +6,6 @@
 var isAlienSorted = function(words, order) {
     
     for (let i = 1; i < words.length; i++) {
-        // let longest = words[i].length > words[i - 1].length ? words[i].length : words[i - 1].length
         for (let j = 0; j < words[i - 1].length; j++){
             if (order.indexOf(words[i][j]) < order.indexOf(words[i - 1][j])) return false
             else if (order.indexOf(words[i][j]) > order.indexOf(words[i - 1][j])) break   
