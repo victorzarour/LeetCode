7d706ce541f8c6c296dcd0ae91520bbfde925d10
Victor Zarour <victor.zarour@gmail.com>
2023-01-30 12:22:54 -0600

    Time: 77 ms (95.31%), Space: 44.1 MB (84.38%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.js
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 var countVowelSubstrings = function(word) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']), temp = new Set()
     let count = 0
-    let vowels = new Set(['a', 'e', 'i', 'o', 'u'])
-    let set = new Set()
     
     for (let i = 0; i < word.length; i++) {
-        set.clear()
-        for (let j = 0; j < word.length; j++) {
+        temp.clear()
+        for (let j = 0; j < word.length; j++){
             if (!vowels.has(word[i + j])) break
-            set.add(word[i + j])
-            if (set.size === vowels.size) count++
+            temp.add(word[i + j])
+            if (temp.size === vowels.size) count++
         }
     }
-    
     return count
 };
