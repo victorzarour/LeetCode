17cfb206088b6ecd738dc82362bf788a319007fc
Victor Zarour <victor.zarour@gmail.com>
2023-02-01 11:06:52 -0600

    Time: 89 ms (47.92%), Space: 47.6 MB (17.71%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {   
+    let small, long
+    if (str1.length > str2.length){
+        small = str2
+        long = str1
+    } else {
+        small = str1
+        long = str2
+    }
+    
+    for (let i = small.length; i >= 0; i--) {
+        const word = small.slice(0, i)
+        const correctSmaller = !small.split(word).join('').length;
+        const correctLarger = !long.split(word).join('').length;
+        if (correctSmaller && correctLarger) return word;
+    }
+    return ""
+};
