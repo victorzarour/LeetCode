0244df899eeeb53214710926991721dfd2a9d10f
Victor Zarour <victor.zarour@gmail.com>
2023-02-07 12:25:53 -0600

    Time: 67 ms (90.53%), Space: 41.7 MB (98.22%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var buddyStrings = function(s, goal) {
+    if (s.length !== goal.length) return false;
+    
+    if (s === goal) {
+        let set = new Set();
+        
+        for (let i = 0; i < s.length; i++) {
+            set.add(s[i]);
+        }
+
+        return set.size < s.length;
+    }
+    
+    let diff = []
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== goal[i]) diff.push(i)
+    }
+    
+    return diff.length === 2 && s[diff[0]] === goal[diff[1]] && s[diff[1]] === goal[diff[0]];
+};
