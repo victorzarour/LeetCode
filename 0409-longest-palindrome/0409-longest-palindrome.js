dfb0aa39da9e5eac60857ad2a89f155b3787b732
Victor Zarour <victor.zarour@gmail.com>
2023-02-10 12:39:59 -0600

    Time: 75 ms (52.80%), Space: 44 MB (40.05%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    const set = new Set();
+    let count = 0;
+    
+    for (let char of s) {
+        if (set.has(char)) {
+			count += 2;
+            set.delete(char);
+        } 
+		else set.add(char);
+    }
+
+    return count + (set.size > 0 ? 1 : 0);
+};
