2e597a7fc0abcec2f9a34ef38502b300f5df4424
Victor Zarour <victor.zarour@gmail.com>
2023-02-14 14:58:21 -0600

    Time: 72 ms (87.14%), Space: 48 MB (15.77%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    let maxDepth = 0;
+    
+    const traverseAndUpdateMaxDepth = (node, depth) => {
+        if (node === null) return depth;
+        depth += 1;
+        
+        if (depth > maxDepth) maxDepth = depth
+        
+        node.children.forEach(child => {
+            traverseAndUpdateMaxDepth(child, depth);
+        })   
+    }
+    
+    traverseAndUpdateMaxDepth(root, 0);
+    
+    return maxDepth;
+};
