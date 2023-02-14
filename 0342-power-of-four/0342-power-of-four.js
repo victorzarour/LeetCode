edadbe5343b60236ead5c99abc5aa147198ca72a
Victor Zarour <victor.zarour@gmail.com>
2023-02-14 14:46:55 -0600

    Time: 80 ms (43.92%), Space: 42.4 MB (80.39%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.js
+++ 0342-power-of-four/0342-power-of-four.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var isPowerOfFour = function(n) {
-    if (n <= 0) return false;
+    if (n <= 0) return false
     
     while (n % 4 === 0) {
-        n /= 4;
+        n /= 4
     }
     
-    return n === 1;
+    return n === 1
 };
