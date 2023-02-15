bafff77ff77f67967c747fbd63aa876b773b67d9
Victor Zarour <victor.zarour@gmail.com>
2023-02-15 09:01:12 -0600

    Time: 116 ms (63.98%), Space: 48.3 MB (33.23%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var addToArrayForm = function(num, k) {
-    // return String(Number(num.join("")) + k).split("").map(num => Number(num)) 
+    return String(BigInt(num.join("")) + BigInt(k)).split("").map(num => Number(num)) 
     
-    const sum = BigInt(num.join("")) + BigInt(k)
+//     const sum = BigInt(num.join("")) + BigInt(k)
     
-    const array = Array.from(String(sum), num => Number(num))
+//     const array = Array.from(String(sum), num => Number(num))
     
-    return array
+    // return array
 //     let string = ""
     
 //     for (let n of num) {
