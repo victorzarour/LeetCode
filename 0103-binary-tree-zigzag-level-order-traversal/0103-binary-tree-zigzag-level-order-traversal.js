81828794866a6769100ec6f41213f52d52359149
Victor Zarour <victor.zarour@gmail.com>
2023-02-19 10:15:32 -0600

    Time: 76 ms (34.42%), Space: 44.3 MB (11.62%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
@@ -12,10 +12,10 @@
  */
 var zigzagLevelOrder = function(root) {
     if (!root) return []
-    const stack = [[ root, 0 ]], vals = []
+    const queue = [[ root, 0 ]], vals = []
     
-    while (stack.length) {
-        const [ current, level ] = stack.shift()
+    while (queue.length) {
+        const [ current, level ] = queue.shift()
         if (!vals[level]) { 
             vals.push([current.val])
         }
@@ -24,34 +24,9 @@ var zigzagLevelOrder = function(root) {
         } 
         
         
-        if (current.right) stack.push([ current.right, level + 1 ])
-        if (current.left) stack.push([ current.left, level + 1 ])
+        if (current.right) queue.push([ current.right, level + 1 ])
+        if (current.left) queue.push([ current.left, level + 1 ])
 
     }
     return vals
 };
-
-// const zigzagLevelOrder = (root) => {
-//     let level = 1;
-//     const out = [];
-//     const queue = [root];
-
-//     while(queue.length) {
-//         const qLen = queue.length;
-//         const innerArr = [];
-//         for(let i = 0; i < qLen; i++) {
-            
-//             const currNode = queue.shift();
-            
-//             if(level % 2 === 0) innerArr.unshift(currNode.val);
-//             else innerArr.push(currNode.val);
-            
-//             if(currNode.left) queue.push(currNode.left);
-//             if(currNode.right) queue.push(currNode.right);
-            
-//         }
-//         level++;
-//         out.push(innerArr);
-//     }
-//     return out;
-// };
