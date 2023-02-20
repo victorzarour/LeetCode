4dfbd1d905e3eef6d5659c2cd177511d3d783586
Victor Zarour <victor.zarour@gmail.com>
2023-02-20 11:20:59 -0600

    Time: 47 ms (99.48%), Space: 41.2 MB (99.89%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -16,24 +16,6 @@ var searchInsert = function(nums, target) {
 };
 
 // [1, 3, 5, 6]
-//        s     
-//        m
-//           l
-
-  
-//     let i = 0;
-//     let middle;
-//     let j = nums.length
-    
-//     while (i <= j){
-//         middle = Math.floor((i + j) / 2);
-        
-//         if (nums[middle] === target) {
-//             return middle
-//         } else if (nums[middle] < target) {
-//             i = middle + 1
-//         } else {
-//             j = middle - 1
-//         }
-//     }
-//     return i
+//     s     
+//     m
+//  l
