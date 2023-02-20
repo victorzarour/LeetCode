25ff147ad933d2207189803963ae9c95b364d477
Victor Zarour <victor.zarour@gmail.com>
2023-02-20 11:18:28 -0600

    Time: 59 ms (85.71%), Space: 41.5 MB (95.91%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,27 +4,36 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-  
-    let i = 0;
-    let middle;
-    let j = nums.length
+    let s = 0, m = Math.floor(nums.length / 2), l = nums.length - 1
     
-    while (i <= j){
-        middle = Math.floor((i + j) / 2);
-        
-        if (nums[middle] === target) {
-            return middle
-        } else if (nums[middle] < target) {
-            i = middle + 1
-        } else {
-            j = middle - 1
-        }
+    while (s <= l) {
+        m = Math.floor((s + l) / 2)
+        if (nums[m] === target) return m
+        else if (nums[m] > target) l = m - 1
+        else s = m + 1
     }
-    return i
-    
+    return s
 };
 
-// nums = [1,3,5,6]   7
-                 // m 
-                 //   i 
-                 //   j
+// [1, 3, 5, 6]
+//        s     
+//        m
+//           l
+
+  
+//     let i = 0;
+//     let middle;
+//     let j = nums.length
+    
+//     while (i <= j){
+//         middle = Math.floor((i + j) / 2);
+        
+//         if (nums[middle] === target) {
+//             return middle
+//         } else if (nums[middle] < target) {
+//             i = middle + 1
+//         } else {
+//             j = middle - 1
+//         }
+//     }
+//     return i
