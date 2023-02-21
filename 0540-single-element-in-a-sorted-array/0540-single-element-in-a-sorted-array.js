bb9d74421855f78c362455f951d105bf983d6bec
Victor Zarour <victor.zarour@gmail.com>
2023-02-21 09:28:38 -0600

    Time: 62 ms (84.83%), Space: 45.3 MB (22.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -3,40 +3,14 @@
  * @return {number}
  */
 
-// var singleNonDuplicate = function(nums) {
-//     let low = 0, high = nums.length - 1
-    
-//     while (low < high) {
-//         let mid = low + (high - low) / 2
-//         if (mid % 2 === 1) mid--
-//         if (nums[mid] === nums[mid + 1]) low = mid + 2
-//         else high = mid
-//     }
-//     return nums[low]
-// }
-
 var singleNonDuplicate = function(nums) {
-    if (nums.length < 1) {
-        return nums;
-    }
+    let low = 0, high = nums.length - 1
     
-    let low = 0;
-    let high = nums.length;
-    let mid;
-    
-    while (low <= high) {
-        mid = low + (Math.floor((high - low) / 2));
-
-        if (nums[mid] !== nums[mid + 1] && nums[mid] !== nums[mid - 1]) {
-            return nums[mid];
-        } else {
-            if( (mid % 2 === 0 && nums[mid] === nums[mid+1]) || 
-                (mid % 2 !== 0 && nums[mid] !== nums[mid + 1])
-              ) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
+    while (low < high) {
+        let mid = (high + low) / 2
+        if (mid % 2 === 1) mid--
+        if (nums[mid] === nums[mid + 1]) low = mid + 2
+        else high = mid
     }
-};
+    return nums[low]
+}
