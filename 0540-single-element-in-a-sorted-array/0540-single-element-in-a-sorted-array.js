dd1d55c13c5473a6b4561a83527d4aece85ac1aa
Victor Zarour <victor.zarour@gmail.com>
2023-02-21 08:46:02 -0600

    Time: 77 ms (27.63%), Space: 44.8 MB (63.81%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -7,7 +7,7 @@ var singleNonDuplicate = function(nums) {
     let low = 0, high = nums.length - 1
     
     while (low < high) {
-        let mid = low + Math.floor((high - low) / 2)
+        let mid = low + (high - low) / 2
         if (mid % 2 === 1) mid--
         if (nums[mid] === nums[mid + 1]) low = mid + 2
         else high = mid
