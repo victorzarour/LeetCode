eff078ccf0a4a5c7166d477822a31634b7468a85
Victor Zarour <victor.zarour@gmail.com>
2023-02-21 08:40:48 -0600

    Time: 59 ms (93.00%), Space: 44.6 MB (87.94%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -3,13 +3,36 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    let map = new Map()
+//     let map = new Map()
     
-    for (let num of nums) {
-        map.set(num, map.get(num) + 1 || 1)
+//     for (let num of nums) {
+//         map.set(num, map.get(num) + 1 || 1)
+//     }
+    
+//     for (let [ key, value ] of map) {
+//         if (value !== 2) return key
+//     } 
+    let low = 0, hi = nums.length - 1
+    
+    while (low < hi) {
+        const mid = low + (hi - low) / 2
     }
+};
+
+var singleNonDuplicate = function(nums) {
+    if (nums.length < 1) return nums;
+    let low = 0, high = nums.length
     
-    for (let [ key, value ] of map) {
-        if (value !== 2) return key
-    } 
+    while (low <= high) {
+        const mid = low + (Math.floor((high - low) / 2));
+
+        if (nums[mid] !== nums[mid + 1] && nums[mid] !== nums[mid - 1]) return nums[mid];
+        else {
+            if( (mid % 2 === 0 && nums[mid] === nums[mid+1]) || 
+                (mid % 2 !== 0 && nums[mid] !== nums[mid + 1])
+              ) {
+                low = mid + 1;
+            } else high = mid - 1;
+        }
+    }
 };
