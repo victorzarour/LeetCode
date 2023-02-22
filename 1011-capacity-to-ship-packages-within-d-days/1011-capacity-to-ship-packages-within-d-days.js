e4662f8088112d1028baa5c7d8ef97dccfcfae5d
Victor Zarour <victor.zarour@gmail.com>
2023-02-22 14:59:27 -0600

    Time: 88 ms (70.43%), Space: 46.4 MB (9.68%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -18,31 +18,23 @@ var shipWithinDays = function(weights, days) {
                 sum = weight
             }
         }
-        
+
         if (count > days) min = mid + 1
         else max = mid        
     }
     return min
 };
     
-//        let low = Math.max(...weights)
-//     let high = weights.reduce((sum, a) => sum + a)
-    
-//     while (low < high) {
-//         let mid = Math.floor((high + low) / 2)
-//         let sum = 0, count = 1
-        
-//         for (let weight of weights) {
-//             sum += weight
-//             if (sum > mid) {
-//                 count++
-//                 sum = weight
-//             }
-//         }
-        
-//         if (count > days) low = mid + 1
-//         else high = mid
-//     }
-//     return low
+// 10 55
+//   32
+
+// 10 32
+//   21
+
+// 10 21
+//   15
+
+// 10 15
+//   12
 
 
