98b346be14c5b2172f39a7bdf6db6d6e6b5abb7f
Victor Zarour <victor.zarour@gmail.com>
2023-02-22 12:29:38 -0600

    Time: 85 ms (77.30%), Space: 45.9 MB (15.14%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -3,24 +3,55 @@
  * @param {number} days
  * @return {number}
  */
-var shipWithinDays = function(weights, days) {
+// var shipWithinDays = function(weights, days) {
     
-    let low = Math.max(...weights)
-    let high = weights.reduce((sum, a) => sum + a)
+//     let low = Math.max(...weights)
+//     let high = weights.reduce((sum, a) => sum + a)
     
-    while (low < high) {
-        let mid = Math.floor((high + low) / 2)
-        let sum = 0, count = 1
+//     while (low < high) {
+//         let mid = Math.floor((high + low) / 2)
+//         let sum = 0, count = 1
         
-        for (let weight of weights) {
-            if (sum + weight > mid) {
-                count++
-                sum = weight
-            } else sum += weight
+//         for (let weight of weights) {
+//             if (sum + weight > mid) {
+//                 count++
+//                 sum = weight
+//             } else sum += weight
+//         }
+        
+//         if (count > days) low = mid + 1
+//         else high = mid
+//     }
+//     return low
+// };
+
+
+//  weights = [1,2,3,4,5,6,7,8,9,10]
+//  10, 55
+
+var shipWithinDays = function(weights, D) {
+    
+    function getDays(capacity) {
+        let days = 1, total = 0;
+        
+        for(let n of weights) {
+            total += n;
+            if(total > capacity) {
+                total = n;
+                days++;
+            }
         }
-        
-        if (count > days) low = mid + 1
-        else high = mid
+        return days;
     }
-    return low
+    
+    let start = Math.max(...weights), 
+        end = weights.reduce((acc, cur) => acc + cur, 0);
+    
+    while(start < end) {
+        const mid = Math.floor((end+start)/2);
+        const days = getDays(mid);
+        if(days > D) start = mid+1;
+        else end = mid;
+    }
+    return end;
 };
