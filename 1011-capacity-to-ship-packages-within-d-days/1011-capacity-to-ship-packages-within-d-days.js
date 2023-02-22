2f36c0a1f4b9cdc46f87da0d210ecb6d7dd86cc9
Victor Zarour <victor.zarour@gmail.com>
2023-02-22 12:31:22 -0600

    Time: 93 ms (54.59%), Space: 46.2 MB (10.81%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -29,29 +29,29 @@ var shipWithinDays = function(weights, days) {
 //  weights = [1,2,3,4,5,6,7,8,9,10]
 //  10, 55
 
-var shipWithinDays = function(weights, D) {
+// var shipWithinDays = function(weights, D) {
     
-    function getDays(capacity) {
-        let days = 1, total = 0;
+//     function getDays(capacity) {
+//         let days = 1, total = 0;
         
-        for(let n of weights) {
-            total += n;
-            if(total > capacity) {
-                total = n;
-                days++;
-            }
-        }
-        return days;
-    }
+//         for(let n of weights) {
+//             total += n;
+//             if(total > capacity) {
+//                 total = n;
+//                 days++;
+//             }
+//         }
+//         return days;
+//     }
     
-    let start = Math.max(...weights), 
-        end = weights.reduce((acc, cur) => acc + cur, 0);
+//     let start = Math.max(...weights), 
+//         end = weights.reduce((acc, cur) => acc + cur, 0);
     
-    while(start < end) {
-        const mid = Math.floor((end+start)/2);
-        const days = getDays(mid);
-        if(days > D) start = mid+1;
-        else end = mid;
-    }
-    return end;
-};
+//     while(start < end) {
+//         const mid = Math.floor((end+start)/2);
+//         const days = getDays(mid);
+//         if(days > D) start = mid+1;
+//         else end = mid;
+//     }
+//     return end;
+// };
