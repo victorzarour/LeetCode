218db2edfadfea90725cc701d42b79ecd3234ead
Victor Zarour <victor.zarour@gmail.com>
2023-02-22 14:48:21 -0600

    Time: 83 ms (80.65%), Space: 45.8 MB (16.13%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -4,12 +4,11 @@
  * @return {number}
  */
 var shipWithinDays = function(weights, days) {
+    let min = Math.max(...weights)
+    let max = weights.reduce((sum, a) => sum + a)
     
-    let low = Math.max(...weights)
-    let high = weights.reduce((sum, a) => sum + a)
-    
-    while (low < high) {
-        let mid = Math.floor((high + low) / 2)
+    while (min < max) {
+        let mid = Math.floor((max + min) / 2)
         let sum = 0, count = 1
         
         for (let weight of weights) {
@@ -20,12 +19,30 @@ var shipWithinDays = function(weights, days) {
             }
         }
         
-        if (count > days) low = mid + 1
-        else high = mid
+        if (count > days) min = mid + 1
+        else max = mid        
     }
-    return low
+    return min
 };
+    
+//        let low = Math.max(...weights)
+//     let high = weights.reduce((sum, a) => sum + a)
+    
+//     while (low < high) {
+//         let mid = Math.floor((high + low) / 2)
+//         let sum = 0, count = 1
+        
+//         for (let weight of weights) {
+//             sum += weight
+//             if (sum > mid) {
+//                 count++
+//                 sum = weight
+//             }
+//         }
+        
+//         if (count > days) low = mid + 1
+//         else high = mid
+//     }
+//     return low
 
 
-//  weights = [1,2,3,4,5,6,7,8,9,10]
-//  10, 55
