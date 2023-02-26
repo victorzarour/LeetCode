4e5d6c503c75c82095674f8b7629ccd8485e6e8e
Victor Zarour <victor.zarour@gmail.com>
2023-02-26 07:44:15 -0600

    Time: 77 ms (87.73%), Space: 45.1 MB (61.55%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.js
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.js
@@ -10,38 +10,18 @@
  * @param {Node|null} root
  * @return {number[]}
  */
-// var preorder = function(root) {
-//     const stack = [ root ], values = []
-    
-//     while (stack.length) {
-//         const current = stack.pop()
-//         values.push(current.val)   
-        
-//         for (let child of current.children) {
-//             stack.push(child)
-//         }
-//     }
-//     return values
-// };
-
 var preorder = function(root) {
-    if (!root){
-        return [];
-    }
+    if (!root) return []
+    const stack = [ root ], values = []
     
-    let order = [];
-    let stack = [];
-    stack.push(root);
-    
-    while(stack.length > 0) {
-        let node = stack.pop();
-        order.push(node.val);
-        // let children = node.children;
-        for (let i = node.children.length - 1; i >= 0; i--) {
-            stack.push(node.children[i]);    
-        }
+    while (stack.length) {
+        const current = stack.pop()
+        values.push(current.val)   
         
+        for (let i = current.children.length - 1; i >= 0; i--) {
+            stack.push(current.children[i]);    
+        }
     }
-    
-    return order;
+    return values
 };
+
