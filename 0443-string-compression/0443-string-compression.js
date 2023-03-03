084832e8cd4fb396cdd00f2646e3c82c75f94a09
Victor Zarour <victor.zarour@gmail.com>
2023-03-03 08:28:59 -0600

    Time: 61 ms (95.50%), Space: 45 MB (19.03%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,22 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    let i = 0, res = 0
+    
+    while (i < chars.length) {
+        let count = 1
+        
+        while (i + count < chars.length && chars[i + count] === chars[i]) count++
+        chars[res++] = chars[i]
+        
+        if (count > 1) {
+            for (let char of String(count).split("")) {
+                chars[res++] = char    
+            }
+        }
+        i += count
+    }
+    return res
+};
