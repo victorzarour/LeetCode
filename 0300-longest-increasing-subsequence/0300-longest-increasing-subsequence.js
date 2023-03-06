4c0d2881250c1ddb9628088ff8838f558b290a26
Victor Zarour <victor.zarour@gmail.com>
2023-03-06 09:52:43 -0600

    Time: 194 ms (49.70%), Space: 43.5 MB (76.27%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums, i = 0, previous = -Infinity, memo = {}) {
+    const table = Array(nums.length).fill(1)
+    let max = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+
+        for (let j = i + 1; j < nums.length; j++) {
+
+            if (nums[j] > nums[i]) {
+                table[j] = Math.max(table[j], 1 + table[i])
+                max = Math.max(max, table[j])
+            }
+        }
+    }
+    return max
+};
