f20d07e32131a234c8be72795f471fc5b021c10c
Victor Zarour <victor.zarour@gmail.com>
2023-03-06 08:37:14 -0600

    Time: 43 ms (100.00%), Space: 42.7 MB (36.60%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -4,11 +4,22 @@
  * @return {number}
  */
 var findKthPositive = function(arr, k) {
-    const array = [], set = new Set(arr)
+//     const array = [], set = new Set(arr)
     
-    for (let i = 1; array.length < k; i++) {
-        if (!set.has(i)) array.push(i)
-    }
+//     for (let i = 1; array.length < k; i++) {
+//         if (!set.has(i)) array.push(i)
+//     }
         
-    return array[k - 1]
+//     return array[k - 1]
+    
+    let left = 0, right = arr.length - 1
+    
+    while (left <= right) {
+        let pivot = Math.floor((right + left) / 2)
+        
+        if (arr[pivot] - pivot - 1 < k) left = pivot + 1
+        else right = pivot - 1
+    }
+    
+    return left + k
 };
