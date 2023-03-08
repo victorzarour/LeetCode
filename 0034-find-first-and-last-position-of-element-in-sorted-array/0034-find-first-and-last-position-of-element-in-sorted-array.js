dd7a8bb7e357e65a330d0db929f16e76ea5ff1fa
Victor Zarour <victor.zarour@gmail.com>
2023-03-08 08:08:25 -0600

    Time: 64 ms (64.34%), Space: 42.6 MB (43.69%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    const find = (nums, target, isFirst) => {
+        let left = 0, right = nums.length - 1
+    
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2)
+
+            if (nums[mid] === target) {
+                if (isFirst) {
+                    if (mid === left || nums[mid - 1] !== target) return mid
+                    right = mid - 1
+                } else {
+                    if (mid === right || nums[mid + 1] !== target) return mid
+                    left = mid + 1
+                }
+            } else if (nums[mid] > target) right = mid - 1
+            else left = mid + 1 
+        }
+        return -1
+    }
+    let first = find(nums, target, true)
+    
+    if (first === -1) return [-1, -1]
+    
+    let last = find(nums, target, false)
+    
+    let left = 0, right = nums.length - 1
+    
+    return [first, last]
+    
+};
+
+
+     
