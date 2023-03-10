582c3752d419dad1f8edd6cb4f23423287ccbad2
Victor Zarour <victor.zarour@gmail.com>
2023-03-10 12:00:59 -0600

    Time: 70 ms (51.54%), Space: 44.7 MB (14.24%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
@@ -3,10 +3,44 @@
  * @return {number}
  */
 var maximumCount = function(nums) {
-    let countNeg = 0, countPos = 0
-    for (let num of nums) {
-        if (num > 0) countPos++
-        else if (num < 0) countNeg++
+    // let countNeg = 0, countPos = 0
+    // for (let num of nums) {
+    //     if (num > 0) countPos++
+    //     else if (num < 0) countNeg++
+    // }
+    // return Math.max(countNeg, countPos)
+
+    const negative = (nums) => {
+        if (nums[0] >= 0 ) return 0;
+        let left = 0, right = nums.length - 1
+
+        while (left < right) {
+            let mid = Math.ceil((left + right) / 2)
+            if (nums[mid] < 0) left = mid
+            else right = mid - 1
+        }
+
+        return left + 1
     }
-    return Math.max(countNeg, countPos)
+    
+    const positive = (nums) => {
+        if (nums[nums.length - 1] <= 0) return 0
+        let left = 0, right = nums.length - 1
+
+        while (left < right) {
+            let mid = Math.floor((left + right) / 2)
+            if (nums[mid] > 0) right = mid
+            else left = mid + 1
+        }
+
+        return nums.length - left
+    }
+    return Math.max(negative(nums), positive(nums))    
 };
+
+
+// [-3,-2,-1,0,0,1,2]
+
+// 0  3  7
+// 0  1  2
+// 1  1  2
