43ea68b557e49a3d31abcbd229cc95db0909e9d9
Victor Zarour <victor.zarour@gmail.com>
2023-03-16 14:26:08 -0600

    Time: 69 ms (68.24%), Space: 44.8 MB (24.63%) - LeetHub

--- 0415-add-strings/0415-add-strings.js
+++ 0415-add-strings/0415-add-strings.js
@@ -11,8 +11,8 @@ var addStrings = function(num1, num2) {
     let carry = 0;
     
     while( i >= 0 || j >= 0 || carry > 0){
-        const digit1 = i < 0 ? 0 : num1.charAt(i) - "0";
-        const digit2 = j < 0 ? 0 : num2.charAt(j) - "0";
+        const digit1 = i < 0 ? 0 : num1[i] - "0";
+        const digit2 = j < 0 ? 0 : num2[j] - "0";
         const digitSum = digit1 + digit2 + carry;
         
         sum = (digitSum >= 10 ? digitSum % 10 : digitSum) + sum;
