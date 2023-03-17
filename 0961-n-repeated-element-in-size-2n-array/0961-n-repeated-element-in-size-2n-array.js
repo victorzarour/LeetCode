81105339d3c720ca0f30152bfb0ff382f2bb8ce9
Victor Zarour <victor.zarour@gmail.com>
2023-03-17 04:38:40 -0600

    Time: 97 ms (37.35%), Space: 45.8 MB (53.40%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
@@ -3,13 +3,19 @@
  * @return {number}
  */
 var repeatedNTimes = function(nums) {
-    const n = nums.length / 2, map = new Map()
+//     const n = nums.length / 2, map = new Map()
     
-    for (let num of nums) {
-        map.set(num, map.get(num) + 1 || 1)
-    }
+//     for (let num of nums) {
+//         map.set(num, map.get(num) + 1 || 1)
+//     }
     
-    for (let [ key, value ] of map) {
-        if (value === n) return key
+//     for (let [ key, value ] of map) {
+//         if (value === n) return key
+//     }
+    
+    nums = nums.sort((a, b) => a - b)
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i + 1]) return nums[i]
     }
 };
