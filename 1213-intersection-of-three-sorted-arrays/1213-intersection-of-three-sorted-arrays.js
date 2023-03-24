cd079042a994569aba7d65b7efc63cf2742d40ac
Victor Zarour <victor.zarour@gmail.com>
2023-03-24 11:03:36 -0600

    Time: 90 ms (28.36%), Space: 50.6 MB (5.97%) - LeetHub

---
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @param {number[]} arr3
+ * @return {number[]}
+ */
+var arraysIntersection = function(arr1, arr2, arr3) {
+    const map = new Map(), array = []
+    
+    for (let num of arr1) {
+        map.set(num, 1)
+    }
+    for (let num of arr2) {
+        map.set(num, map.get(num) + 1 || 1)
+    }
+    for (let num of arr3) {
+        map.set(num, map.get(num) + 1 || 1)
+    }
+    console.log(map)
+    for (let [ key, value ] of map) {
+        if (value === 3) array.push(key)
+    }
+    
+    
+    return array 
+};
