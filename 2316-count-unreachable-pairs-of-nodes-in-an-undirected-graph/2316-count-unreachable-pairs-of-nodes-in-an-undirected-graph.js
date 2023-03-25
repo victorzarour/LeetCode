62a366eae710d5952f52c21463f4edf9f5d29753
Victor Zarour <victor.zarour@gmail.com>
2023-03-25 07:06:12 -0600

    Time: 1386 ms (23.33%), Space: 137.1 MB (6.67%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.js
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.js
@@ -5,23 +5,24 @@
  */
 
 var countPairs = function (n, edges) {
-  const graph = new Array(n).fill().map((_) => []);
+  const graph = buildGraph(edges, n)
     
-  for (const [v1, v2] of edges) {
-    graph[v1].push(v2);
-    graph[v2].push(v1);
-  }
+//   for (const [v1, v2] of edges) {
+//     graph[v1].push(v2);
+//     graph[v2].push(v1);
+//   }
     console.log(graph)
 
   const counts = [],  visited = new Set();
     
-  for (let i = 0; i < n; i++) {
-    if (visited.has(i)) continue;
-    visited.add(i);
-    const count = dfs(i, graph, visited);
+  for (let node in graph) {
+    if (visited.has(String(node))) continue;
+    visited.add(String(node));
+    const count = dfs(node, graph, visited);
     counts.push(count);
   }
 
+console.log(counts)
   let ans = 0, sum = counts[0];
   for (let i = 1; i < counts.length; i++) {
     ans += sum * counts[i];
@@ -35,10 +36,25 @@ function dfs(node, graph, visited) {
   let count = 1;
     
   for (const child of graph[node]) {
-    if (visited.has(child)) continue;
-    visited.add(child);
+    if (visited.has(String(child))) continue;
+    visited.add(String(child));
     count += dfs(child, graph, visited);
   }
 
   return count;
+}
+
+const buildGraph = (edges, n) => {
+  const graph = {}
+  
+  for (let i = 0; i < n; i++) {
+      graph[i] = []
+  }
+  
+  for (let [ a, b ] of edges){
+    graph[a].push(b)
+    graph[b].push(a)
+  }
+  
+  return graph
 }
