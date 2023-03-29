d18aed82a49ac535486d24d5d4484a4941204177
Victor Zarour <victor.zarour@gmail.com>
2023-03-29 10:19:57 -0400

    Time: 71 ms (45.30%), Space: 44.5 MB (23.93%) - LeetHub

---
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var trimMean = function(arr) {
+
+    // let five = arr.length / 20
+    // arr = arr.sort((a, b) => a - b).slice(five + 1, arr.length - five)
+    // return arr.reduce((a, b) => a + b) / arr.length
+    
+        arr.sort((a, b) => a - b)
+
+    let fpl = arr.length * 0.05 //Abbreviates five percent of length
+    let sum = 0;
+
+    for (let i = fpl; i < arr.length - fpl; i++) sum += arr[i]
+
+    return sum / (arr.length - (fpl * 2))
+};
