de420e6fc3eac3adcd0a96905f842e5e3ed747b7
Victor Zarour <victor.zarour@gmail.com>
2023-03-29 10:07:15 -0400

    Time: 80 ms (8.05%), Space: 45.3 MB (6.90%) - LeetHub

--- 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.js
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.js
@@ -4,17 +4,15 @@
  * @return {boolean}
  */
 var canFormArray = function(arr, pieces) {
+    const map = new Map()
+    for (let piece of pieces) {
+        map.set(piece[0], piece)
+    }
+    console.log(map)
     let i = 0
     while (i < arr.length) {
-        let found = -1
-        for (let j = 0; j < pieces.length; j++) {
-            if (arr[i] === pieces[j][0]) {
-                found = j
-                break;
-            }
-        }
-        if (found === -1) return false
-        let target = pieces[found]
+        if (!map.get(arr[i])) return false
+        let target = map.get(arr[i])
         for (let x of target) {
             if (x !== arr[i]) return false
             i++
