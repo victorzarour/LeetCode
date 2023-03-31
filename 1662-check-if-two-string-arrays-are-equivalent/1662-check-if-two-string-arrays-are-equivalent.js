0e713c14a6067ddfa50c95ac379d109901e12e14
Victor Zarour <victor.zarour@gmail.com>
2023-03-31 10:35:23 -0400

    Time: 61 ms (55.54%), Space: 42.5 MB (12.34%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -4,5 +4,13 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function(word1, word2) {
-    return word1.join("") === word2.join("")   
+    // return word1.join("") === word2.join("") 
+    let string1 = "", string2 = ""
+    for (let letter of word1) {
+        string1 += letter
+    }
+    for (let letter of word2) {
+        string2 += letter
+    }
+    return string1 === string2
 };
