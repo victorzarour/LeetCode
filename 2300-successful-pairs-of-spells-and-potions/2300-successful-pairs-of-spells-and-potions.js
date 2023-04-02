aaf94ec5bc3c1ee54a3da7e9be0c04c4b935f588
Victor Zarour <victor.zarour@gmail.com>
2023-04-02 10:59:49 -0400

    Time: 340 ms (33.33%), Space: 79.2 MB (6.67%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
@@ -6,33 +6,31 @@
  */
 var successfulPairs = function(spells, potions, success) {
     let res = [];
-    potions.sort((a, b) => b-a);
+    potions.sort((a, b) => b - a);
     let map = new Map();
     
-    for(let i=0; i<spells.length; i++){
-        if(!map.has(spells[i])){
+    for(let i = 0; i < spells.length; i++){
+        if (!map.has(spells[i])) {
             let s = success / spells[i];
             let len = search(potions, s);
             res.push(len);
             map.set(spells[i], len);
-        }else{
+        } else{
             let len = map.get(spells[i]);
             res.push(len);
         }
     }
-    
     return res;
 };
 
 function search(potions, target){
     let res = 0;
     let left = 0;
-    let right = potions.length-1;
+    let right = potions.length - 1;
     while(left <= right){
         let mid = Math.floor((left + right) / 2);
-        if(potions[mid] < target){
-            right = mid - 1;
-        }else{
+        if (potions[mid] < target) right = mid - 1;
+        else {
             left = mid + 1;
             res = mid + 1;
         }
