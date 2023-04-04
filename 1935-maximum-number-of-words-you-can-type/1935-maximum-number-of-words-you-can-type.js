a577e419f2e77711fd750664b02b285c70e13201
Victor Zarour <victor.zarour@gmail.com>
2023-04-04 13:02:31 -0400

    Time: 66 ms (46.88%), Space: 42.2 MB (83.13%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} text
+ * @param {string} brokenLetters
+ * @return {number}
+ */
+var canBeTypedWords = function(text, brokenLetters) {
+    let words = text.split(' ');
+    let set = new Set(brokenLetters);
+    let count = 0;
+    for(let i =0; i< words.length; i++){
+        let word = words[i];
+        for(let j =0; j<word.length; j++){
+            if(set.has(word[j])){
+                count++;
+                break;
+            }
+        }
+    }
+     return words.length - count;
+};
