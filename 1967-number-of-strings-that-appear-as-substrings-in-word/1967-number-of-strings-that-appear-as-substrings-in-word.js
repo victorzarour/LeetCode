fc0aab03212fa92578c4614007cda774e9e305a9
Victor Zarour <victor.zarour@gmail.com>
2023-04-05 12:41:25 -0400

    Time: 58 ms (74.47%), Space: 42.4 MB (31.91%) - LeetHub

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+var numOfStrings = function(patterns, word) {
+    let count = 0
+    for (let pattern of patterns) {
+        if (word.includes(pattern)) count++
+    }
+    
+    return count
+};
