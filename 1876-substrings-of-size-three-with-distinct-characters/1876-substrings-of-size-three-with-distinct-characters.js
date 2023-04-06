2bf2b95ae8920ffdb0cc45ecf3cfe60c1ebe9a98
Victor Zarour <victor.zarour@gmail.com>
2023-04-06 09:51:40 -0400

    Time: 138 ms (6.16%), Space: 48.5 MB (7.19%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countGoodSubstrings = function(s) {
+    let substrings = [], count = 0
+    for (let i = 0; i < s.length - 2; i++) {
+        let substring = s[i] + s[i + 1] + s[i + 2]
+        substrings.push(substring)
+    }
+    
+    for (let substring of substrings) {
+        console.log(substring)
+        if (substring[0] !== substring[1] && substring[1] !== substring[2] && substring[0] !== substring[2]) count++
+    }
+    return count
+};
