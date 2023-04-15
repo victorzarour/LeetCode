d78a4db936dd89b10049ace5114a0762918d6435
Victor Zarour <victor.zarour@gmail.com>
2023-04-15 19:46:30 +0100

    Time: 69 ms (16.48%), Space: 44.3 MB (14.88%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -5,8 +5,8 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    for (let i = 0; i < nums.length; i++) {
-        init = fn(init, nums[i])
+    for (let num of nums) {
+        init = fn(init, num)
     }
     return init
 };
